b52ed6d4c5138f0ca554509e8e1935c8a1073d8b
Gerard <Gerard@.(none)>
2015-10-18 10:34:26 +0100

    Generalise two of the tests in 'BitemporalSpec' to check against specific flavours of item type. Also a minor tweak to the test for monad-plus behaviour.

--- src/test/scala-2.11/com/sageserpent/plutonium/BitemporalSpec.scala
+++ src/test/scala-2.11/com/sageserpent/plutonium/BitemporalSpec.scala
@@ -6,6 +6,7 @@ import Prop.BooleanOperators
 import org.scalatest.FlatSpec
 import org.scalatest.prop.Checkers
 
+import scala.reflect.runtime.universe._
 import scala.util.Random
 import scalaz.scalacheck._
 import scalaz.{Equal, MonadPlus}
@@ -54,11 +55,11 @@ class BitemporalSpec extends FlatSpec with Checkers with WorldSpecSupport {
           Gen.frequency(5 -> (Arbitrary.arbitrary[Int] map (MonadPlus[Bitemporal].point(_))),
             10 -> (Gen.oneOf(ids) map (Bitemporal.zeroOrOneOf[IntegerHistory](_) map (_.integerProperty))),
             10 -> (Gen.oneOf(idsInExistence) map (Bitemporal.singleOneOf[IntegerHistory](_) map (_.integerProperty))),
-            10 -> (Gen.oneOf(idsInExistence) map (Bitemporal.withId[IntegerHistory](_) map (_.integerProperty))),
+            10 -> (Gen.oneOf(ids) map (Bitemporal.withId[IntegerHistory](_) map (_.integerProperty))),
             3 -> Gen.const(Bitemporal.wildcard[IntegerHistory] map (_.integerProperty)),
             10 -> (Gen.oneOf(ids) map (Bitemporal.zeroOrOneOf[IntegerHistory](_) map intFrom)),
             10 -> (Gen.oneOf(idsInExistence) map (Bitemporal.singleOneOf[IntegerHistory](_) map intFrom)),
-            10 -> (Gen.oneOf(idsInExistence) map (Bitemporal.withId[IntegerHistory](_) map intFrom)),
+            10 -> (Gen.oneOf(ids) map (Bitemporal.withId[IntegerHistory](_) map intFrom)),
             3 -> Gen.const(Bitemporal.wildcard[IntegerHistory] map intFrom),
             1 -> Gen.const(Bitemporal.none[Int]))
         )
@@ -117,6 +118,7 @@ class BitemporalSpec extends FlatSpec with Checkers with WorldSpecSupport {
                                  asOfs <- Gen.listOfN(bigShuffledHistoryOverLotsOfThings.length, instantGenerator) map (_.sorted)
                                  queryWhen <- unboundedInstantGenerator
     } yield (recordingsGroupedById, bigShuffledHistoryOverLotsOfThings, asOfs, queryWhen)
+
     check(Prop.forAllNoShrink(testCaseGenerator) { case (recordingsGroupedById, bigShuffledHistoryOverLotsOfThings, asOfs, queryWhen) =>
       val world = new WorldUnderTest()
 
@@ -124,14 +126,23 @@ class BitemporalSpec extends FlatSpec with Checkers with WorldSpecSupport {
 
       val scope = world.scopeFor(queryWhen, world.nextRevision)
 
-      val ids = (recordingsGroupedById map { case RecordingsForAnId(historyId, _, _, _) => historyId.asInstanceOf[History#Id] }).toSet
+      def holdsFor[AHistory <: History : TypeTag]: Prop = {
+        // The filtering of ids here is hokey - disjoint history types can (actually, they do) share the same id type, so we'll
+        // end up with ids that may be irrelevant to the flavour of 'AHistory' we are checking against. This doesn't matter, though,
+        // because the queries we are cross checking allow the possibility that there are no items of the specific type to match them.
+        val ids = (recordingsGroupedById map { case RecordingsForAnId(historyId, _, _, _) => historyId } filter (_.isInstanceOf[AHistory#Id]) map (_.asInstanceOf[AHistory#Id])).toSet
 
-      val itemsFromWildcardQuery = scope.render(Bitemporal.wildcard[History]) toSet
+        val itemsFromWildcardQuery = scope.render(Bitemporal.wildcard[AHistory]) toSet
 
-      Prop.all(ids.toSeq map (id => {
-        val itemsFromSpecificQuery = scope.render(Bitemporal.withId[History](id)).toSet
-        itemsFromSpecificQuery.subsetOf(itemsFromWildcardQuery) :| s"itemsFromSpecificQuery.subsetOf(${itemsFromWildcardQuery})"
-      }): _*)
+        Prop.all(ids.toSeq map (id => {
+          val itemsFromSpecificQuery = scope.render(Bitemporal.withId[AHistory](id)).toSet
+          itemsFromSpecificQuery.subsetOf(itemsFromWildcardQuery) :| s"itemsFromSpecificQuery.subsetOf(${itemsFromWildcardQuery})"
+        }): _*)
+      }
+
+      holdsFor[History] && holdsFor[BarHistory] &&
+        holdsFor[FooHistory] && holdsFor[IntegerHistory] &&
+        holdsFor[MoreSpecificFooHistory]
     })
   }
 
@@ -154,21 +165,29 @@ class BitemporalSpec extends FlatSpec with Checkers with WorldSpecSupport {
 
       val scope = world.scopeFor(queryWhen, world.nextRevision)
 
-      val ids = (recordingsGroupedById map { case RecordingsForAnId(historyId, _, _, _) => historyId.asInstanceOf[History#Id] }).toSet
+      def holdsFor[AHistory <: History : TypeTag]: Prop = {
+        // The filtering of ids here is hokey - disjoint history types can (actually, they do) share the same id type, so we'll
+        // end up with ids that may be irrelevant to the flavour of 'AHistory' we are checking against. This doesn't matter, though,
+        // because the queries we are cross checking allow the possibility that there are no items of the specific type to match them.
+        val ids = (recordingsGroupedById map { case RecordingsForAnId(historyId, _, _, _) => historyId } filter (_.isInstanceOf[AHistory#Id]) map (_.asInstanceOf[AHistory#Id])).toSet
 
-      Prop.all(ids.toSeq map (id => {
-        val itemsFromGenericQuery = scope.render(Bitemporal.withId[History](id)).toSet
-        (if (2 > itemsFromGenericQuery.size) {
-          val itemsFromZeroOrOneOfQuery = scope.render(Bitemporal.zeroOrOneOf[History](id)).toSet
-          (itemsFromGenericQuery == itemsFromZeroOrOneOfQuery) :| s"${itemsFromGenericQuery} == itemsFromZeroOrOneOfQuery"
-        }
-        else Prop.proved) && (if (1 == itemsFromGenericQuery.size) {
-          val itemsFromSingleOneOfQuery = scope.render(Bitemporal.singleOneOf[History](id)).toSet
-          (itemsFromGenericQuery == itemsFromSingleOneOfQuery) :| s"${itemsFromGenericQuery} == itemsFromSingleOneOfQuery"
-        }
-        else Prop.proved)
-      }): _*)
+        Prop.all(ids.toSeq map (id => {
+          val itemsFromGenericQuery = scope.render(Bitemporal.withId[History](id)).toSet
+          (if (2 > itemsFromGenericQuery.size) {
+            val itemsFromZeroOrOneOfQuery = scope.render(Bitemporal.zeroOrOneOf[History](id)).toSet
+            (itemsFromGenericQuery == itemsFromZeroOrOneOfQuery) :| s"${itemsFromGenericQuery} == itemsFromZeroOrOneOfQuery"
+          }
+          else Prop.proved) && (if (1 == itemsFromGenericQuery.size) {
+            val itemsFromSingleOneOfQuery = scope.render(Bitemporal.singleOneOf[History](id)).toSet
+            (itemsFromGenericQuery == itemsFromSingleOneOfQuery) :| s"${itemsFromGenericQuery} == itemsFromSingleOneOfQuery"
+          }
+          else Prop.proved)
+        }): _*)
+      }
 
+      holdsFor[History] && holdsFor[BarHistory] &&
+        holdsFor[FooHistory] && holdsFor[IntegerHistory] &&
+        holdsFor[MoreSpecificFooHistory]
     })
   }
 
