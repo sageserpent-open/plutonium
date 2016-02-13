ddc5d1b4aa59c8ab4cfdd6247b25a9b1f937de05
Gerard <Gerard@.(none)>
2016-02-13 14:06:29 +0000

    WIP: Realised that the existing test was really turning into two separate tests...

--- src/test/scala-2.11/com/sageserpent/plutonium/ExperimentalWorldSpec.scala
+++ src/test/scala-2.11/com/sageserpent/plutonium/ExperimentalWorldSpec.scala
@@ -17,7 +17,7 @@ class ExperimentalWorldSpec extends FlatSpec with Matchers with Checkers with Wo
   implicit override val generatorDrivenConfig =
     PropertyCheckConfig(maxSize = 30)
 
-  "An experimental world" should "respond to scope queries in the same way as it parent world as long as the scope for querying is contained within the defining scope" in {
+  "An experimental world" should "respond to scope queries in the same way as its base world as long as the scope for querying is contained within the defining scope" in {
     val testCaseGenerator = for {baseWorld <- worldGenerator
                                  recordingsGroupedById <- recordingsGroupedByIdGenerator(forbidAnnihilations = false)
                                  seed <- seedGenerator
@@ -49,4 +49,50 @@ class ExperimentalWorldSpec extends FlatSpec with Matchers with Checkers with Wo
       ((utopianHistory.length == distopianHistory.length) :| s"${utopianHistory.length} == distopianHistory.length") && Prop.all(utopianHistory zip distopianHistory map { case (utopianCase, distopianCase) => (utopianCase === distopianCase) :| s"${utopianCase} === distopianCase" }: _*)
     })
   }
+
+  it should "be unaffected by subsequent revisions of its base world" in {
+    val testCaseGenerator = for {baseWorld <- worldGenerator
+                                 recordingsGroupedById <- recordingsGroupedByIdGenerator(forbidAnnihilations = false)
+                                 followingRecordingsGroupedById <- nonConflictingRecordingsGroupedByIdGenerator
+                                 seed <- seedGenerator
+                                 random = new Random(seed)
+                                 bigShuffledHistoryOverLotsOfThings = (random.splitIntoNonEmptyPieces(shuffleRecordingsPreservingRelativeOrderOfEventsAtTheSameWhen(random, recordingsGroupedById)
+                                   .zipWithIndex)).force
+                                 bigFollowingShuffledHistoryOverLotsOfThings = (random.splitIntoNonEmptyPieces(shuffleRecordingsPreservingRelativeOrderOfEventsAtTheSameWhen(random, recordingsGroupedById)
+                                   .zipWithIndex)).force
+                                 baseHistoryLength = bigShuffledHistoryOverLotsOfThings.length
+                                 followingHistoryLength = bigFollowingShuffledHistoryOverLotsOfThings.length
+                                 asOfs <- Gen.listOfN(baseHistoryLength + followingHistoryLength, instantGenerator) map (_.sorted)
+                                 (baseAsOfs, followingAsOfs) = asOfs splitAt baseHistoryLength
+                                 forkAsOf <- instantGenerator
+                                 forkWhen <- unboundedInstantGenerator
+                                 queryAsOf <- instantGenerator if !forkAsOf.isBefore(queryAsOf)
+                                 queryWhen <- unboundedInstantGenerator if queryWhen <= forkWhen
+    } yield (baseWorld, recordingsGroupedById, bigShuffledHistoryOverLotsOfThings, bigFollowingShuffledHistoryOverLotsOfThings, baseAsOfs, followingAsOfs, forkAsOf, forkWhen, queryAsOf, queryWhen)
+    check(Prop.forAllNoShrink(testCaseGenerator) { case (baseWorld, recordingsGroupedById, bigShuffledHistoryOverLotsOfThings, bigFollowingShuffledHistoryOverLotsOfThings, baseAsOfs, followingAsOfs, forkAsOf, forkWhen, queryAsOf, queryWhen) =>
+      recordEventsInWorld(liftRecordings(bigShuffledHistoryOverLotsOfThings), baseAsOfs, baseWorld)
+
+      val scopeToDefineFork = baseWorld.scopeFor(forkWhen, forkAsOf)
+
+      val experimentalWorld = baseWorld.forkExperimentalWorld(scopeToDefineFork)
+
+      // There is a subtlety here - the first of the following asOfs may line up with the last or the original asOfs
+      // - however the experimental world should still remain unperturbed.
+      recordEventsInWorld(liftRecordings(bigFollowingShuffledHistoryOverLotsOfThings), followingAsOfs, baseWorld)
+
+      assert(baseWorld.nextRevision == experimentalWorld.nextRevision)
+      assert(baseWorld.revisionAsOfs == experimentalWorld.revisionAsOfs)
+
+      val scopeFromBaseWorld = baseWorld.scopeFor(queryWhen, queryAsOf)
+      val scopeFromExperimentalWorld = experimentalWorld.scopeFor(queryWhen, queryAsOf)
+
+      val utopianHistory = historyFrom(baseWorld, recordingsGroupedById)(scopeFromBaseWorld)
+      val distopianHistory = historyFrom(experimentalWorld, recordingsGroupedById)(scopeFromExperimentalWorld)
+
+      ((utopianHistory.length == distopianHistory.length) :| s"${utopianHistory.length} == distopianHistory.length") && Prop.all(utopianHistory zip distopianHistory map { case (utopianCase, distopianCase) => (utopianCase === distopianCase) :| s"${utopianCase} === distopianCase" }: _*)
+    })
+  }
 }
+
+
+
