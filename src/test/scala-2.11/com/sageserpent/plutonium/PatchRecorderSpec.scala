c5e46d00aa54634f45c3257ce3d3bd993d3dfdff
Gerard <>
2016-04-20 15:17:53 +0100

    WIP: more slogging...

--- src/test/scala-2.11/com/sageserpent/plutonium/PatchRecorderSpec.scala
+++ src/test/scala-2.11/com/sageserpent/plutonium/PatchRecorderSpec.scala
@@ -1,8 +1,9 @@
 package com.sageserpent.plutonium
 
+import java.lang.reflect.Method
 import java.time.Instant
 
-import com.sageserpent.americium.PositiveInfinity
+import com.sageserpent.americium.{Finite, PositiveInfinity}
 import com.sageserpent.americium.randomEnrichment._
 import com.sageserpent.plutonium.WorldReferenceImplementation.IdentifiedItemsScopeImplementation
 import org.scalacheck.{Gen, Prop}
@@ -21,6 +22,8 @@ import scala.util.Random
 class PatchRecorderSpec extends FlatSpec with Matchers with Checkers with MockFactory with WorldSpecSupport {
   type RecordingActionFactory = (Instant) => RecordingAction
 
+  type PatchesOfTheSameKindForAnId = (Seq[RecordingActionFactory], Set[AbstractPatch])
+
   type LifecycleForAnId = (Seq[RecordingActionFactory], Set[AbstractPatch])
 
   type LifecyclesForAnId = (Seq[RecordingActionFactory], Set[AbstractPatch])
@@ -30,10 +33,49 @@ class PatchRecorderSpec extends FlatSpec with Matchers with Checkers with MockFa
   case class TestCase(recordingActions: Seq[RecordingAction],
                       patchesThatAreExpectedToBeApplied: Set[AbstractPatch])
 
-  def lifecycleForAnId(id: FooHistory#Id): Gen[LifecycleForAnId] = ???
+  val fooClazz = classOf[FooHistory]
 
-  def finiteLifecycleForAnId(id: FooHistory#Id): Gen[LifecycleForAnId] = for {
-    (recordingActionFactories, patchesThatAreExpectedToBeApplied) <- lifecycleForAnId(id)
+  val fooProperty1 = fooClazz.getMethod("property1_$eq")
+  val fooProperty2 = fooClazz.getMethod("property2_$eq")
+
+  def patchGenerator(id: FooHistory#Id, method: Method): Gen[AbstractPatch] = ???
+
+  def patchesOfTheSameKindForAnId(id: FooHistory#Id, seed: Long): Gen[PatchesOfTheSameKindForAnId] = for {
+    method <- Gen.oneOf(fooProperty1, fooProperty2)
+    patches <- Gen.nonEmptyListOf(patchGenerator(id, method))
+    initialPatchInLifecycleIsChange <- Gen.oneOf(false, true)
+  } yield {
+    val randomBehaviour = new Random(seed)
+    val clumpsOfPatches = randomBehaviour.splitIntoNonEmptyPieces(patches)
+    val patchesThatAreExpectedToBeApplied = (clumpsOfPatches map (randomBehaviour.chooseOneOf(_))).toSet
+
+    def recordingChange(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
+      patchRecorder.recordPatchFromChange(Finite(when), patch)
+    }
+
+    def recordingMeasurement(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
+      patchRecorder.recordPatchFromMeasurement(Finite(when), patch)
+    }
+
+    val changeInsteadOfMeasurementDecisionsInClumps = (initialPatchInLifecycleIsChange +: Stream.continually(false)) +: Stream.continually(true +: Stream.continually(false))
+
+    val recordingActionFactories = clumpsOfPatches zip changeInsteadOfMeasurementDecisionsInClumps map
+      {case (clumpOfPatches, decisions) => clumpOfPatches.toSeq zip decisions map {case (patch, makeAChange) => if (makeAChange) recordingChange(patch) _ else recordingMeasurement(patch) _}}
+
+    recordingActionFactories.flatten -> patchesThatAreExpectedToBeApplied
+  }
+
+  def lifecycleForAnId(id: FooHistory#Id, seed: Long): Gen[LifecycleForAnId] = for {
+    (recordingActionFactoriesOverSeveralKinds, patchesThatAreExpectedToBeAppliedOverSeveralKinds) <- Gen.listOf(patchesOfTheSameKindForAnId(id, seed)) map (_.unzip)
+  } yield {
+    val randomBehaviour = new Random(seed)
+    val recordingActionFactories = randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralKinds)
+    val patchesThatAreExpectedToBeApplied = patchesThatAreExpectedToBeAppliedOverSeveralKinds.toSet.flatten;
+    recordingActionFactories -> patchesThatAreExpectedToBeApplied
+  }
+
+  def finiteLifecycleForAnId(id: FooHistory#Id, seed: Long): Gen[LifecycleForAnId] = for {
+    (recordingActionFactories, patchesThatAreExpectedToBeApplied) <- lifecycleForAnId(id, seed)
   } yield {
     def recordingFinalAnnihilation(when: Instant)(patchRecorder: PatchRecorder): Unit = {
       patchRecorder.recordAnnihilation[FooHistory](when, id)
@@ -42,9 +84,9 @@ class PatchRecorderSpec extends FlatSpec with Matchers with Checkers with MockFa
     (recordingActionFactories :+ (recordingFinalAnnihilation _)) -> patchesThatAreExpectedToBeApplied
   }
 
-  def lifecyclesForAnId(id: FooHistory#Id): Gen[LifecyclesForAnId] = for {
-    (recordingsItemFactoriesForFiniteLifecycles, patchesThatAreExpectedToBeAppliedForFiniteLifecycles) <- Gen.listOf(finiteLifecycleForAnId(id)) map (_.unzip)
-    finalUnboundedLifecycle <- Gen.option(finiteLifecycleForAnId(id))
+  def lifecyclesForAnId(id: FooHistory#Id, seed: Long): Gen[LifecyclesForAnId] = for {
+    (recordingsItemFactoriesForFiniteLifecycles, patchesThatAreExpectedToBeAppliedForFiniteLifecycles) <- Gen.listOf(finiteLifecycleForAnId(id, seed)) map (_.unzip)
+    finalUnboundedLifecycle <- Gen.option(finiteLifecycleForAnId(id, seed))
   } yield {
     val recordingActionFactories = recordingsItemFactoriesForFiniteLifecycles reduce (_ ++ _)
     val patchesThatAreExpectedToBeApplied = patchesThatAreExpectedToBeAppliedForFiniteLifecycles.toSet.flatten
@@ -59,12 +101,12 @@ class PatchRecorderSpec extends FlatSpec with Matchers with Checkers with MockFa
 
   def recordingActionFactoriesGenerator(seed: Long): Gen[(Seq[RecordingActionFactory], Set[AbstractPatch])] = for {
     ids <- Gen.containerOf[Set, FooHistory#Id](fooHistoryIdGenerator)
-    (recordingActionFactoriesOverSeveralIds, patchesThatAreExpectedToBeAppliedOverSeveralIds) <- Gen.sequence[Seq[LifecyclesForAnId], LifecyclesForAnId](ids.toSeq map (lifecyclesForAnId(_))) map (_.unzip)
-    randomBehaviour = new Random(seed)
+    (recordingActionFactoriesOverSeveralIds, patchesThatAreExpectedToBeAppliedOverSeveralIds) <- Gen.sequence[Seq[LifecyclesForAnId], LifecyclesForAnId](ids.toSeq map (lifecyclesForAnId(_, seed))) map (_.unzip)
   } yield {
-    val recordingItemFactories = randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralIds)
+    val randomBehaviour = new Random(seed)
+    val recordingActionFactories = randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralIds)
     val patchesThatAreExpectedToBeApplied = patchesThatAreExpectedToBeAppliedOverSeveralIds.toSet.flatten;
-    recordingItemFactories -> patchesThatAreExpectedToBeApplied
+    recordingActionFactories -> patchesThatAreExpectedToBeApplied
   }
 
   val testCaseGenerator: Gen[TestCase] =
