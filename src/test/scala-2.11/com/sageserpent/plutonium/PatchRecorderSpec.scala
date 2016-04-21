ca7b2e493ddfdee9f56a3ce43aa307212dca99d3
Gerard <>
2016-04-21 16:39:27 +0100

    Get rid of the lifted lambdas in the test case generation, they are always the same objects.

--- src/test/scala-2.11/com/sageserpent/plutonium/PatchRecorderSpec.scala
+++ src/test/scala-2.11/com/sageserpent/plutonium/PatchRecorderSpec.scala
@@ -51,133 +51,119 @@ class PatchRecorderSpec extends FlatSpec with Matchers with Checkers with MockFa
     mock[WorkaroundToMakeAbstractPatchMockable]
   }) map (_.apply)
 
-  def patchesOfTheSameKindForAnIdGenerator(id: FooHistory#Id,
-                                           seed: Long,
-                                           bestPatchSelection: BestPatchSelection,
-                                           eventsHaveEffectNoLaterThan: Unbounded[Instant],
-                                           patchGenerator: FooHistory#Id => Gen[AbstractPatch]): Gen[PatchesOfTheSameKindForAnId] = for {
-    patches <- Gen.nonEmptyListOf(patchGenerator(id))
-    initialPatchInLifecycleIsChange <- Gen.oneOf(false, true)
-  } yield {
-    val randomBehaviour = new Random(seed)
-    val clumpsOfPatches = randomBehaviour.splitIntoNonEmptyPieces(patches).force
-
-    val (setupInteractionsWithBestPatchSelection, bestPatches) =
-      (for (clumpOfPatches <- clumpsOfPatches) yield {
-        val bestPatch = randomBehaviour.chooseOneOf(clumpOfPatches)
-
-        def setupInteractionWithBestPatchSelection() {
-          (bestPatchSelection.apply _).expects(clumpOfPatches.toSeq).returns(bestPatch).once
-        }
-
-        setupInteractionWithBestPatchSelection _ -> bestPatch
-      }) unzip
-
-    inSequence {
-      for (setupInteraction <- setupInteractionsWithBestPatchSelection) {
-        setupInteraction()
-      }
-    }
-
-    val changeInsteadOfMeasurementDecisionsInClumps = (initialPatchInLifecycleIsChange #:: Stream.continually(false)) +: Stream.continually(true #:: Stream.continually(false))
-
-    val recordingActionFactories = clumpsOfPatches zip bestPatches zip changeInsteadOfMeasurementDecisionsInClumps map {
-      case ((clumpOfPatches, bestPatch), decisions)  =>
-        // HACK: this next variable and how it is used is truly horrible...
-        var bestPatchIsExpectedToBeAppliedNoLaterThanCutoff: Option[Boolean] = None
-        clumpOfPatches.toSeq zip decisions map {
-          case (patch, makeAChange) =>
-            def setupInteractionWithBestPatchApplication(patch: AbstractPatch, eventsHaveEffectNoLaterThan: Unbounded[Instant], when: Instant): Unit = {
-              bestPatchIsExpectedToBeAppliedNoLaterThanCutoff match {
-                case None => bestPatchIsExpectedToBeAppliedNoLaterThanCutoff = Some(Finite(when) <= eventsHaveEffectNoLaterThan)
-                case Some(_) =>
-              }
-
-              if (bestPatchIsExpectedToBeAppliedNoLaterThanCutoff.get && bestPatches.contains(patch)) {
-                (patch.apply _).expects(*).once
-                (patch.checkInvariant _).expects(*).once
-              } else {
-                (patch.apply _).expects(*).never
-                (patch.checkInvariant _).expects(*).never
-              }
-            }
-
-            def recordingChange(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
-              setupInteractionWithBestPatchApplication(patch, eventsHaveEffectNoLaterThan, when)
-              patchRecorder.recordPatchFromChange(Finite(when), patch)
-            }
-
-            def recordingMeasurement(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
-              setupInteractionWithBestPatchApplication(patch, eventsHaveEffectNoLaterThan, when)
-              patchRecorder.recordPatchFromMeasurement(Finite(when), patch)
-            }
-            if (makeAChange) recordingChange(patch) _ else recordingMeasurement(patch) _
-        }
-    }
-
-    recordingActionFactories.flatten
-  }
-
-  def lifecycleForAnIdGenerator(id: FooHistory#Id,
-                                seed: Long,
-                                bestPatchSelection: BestPatchSelection,
-                                eventsHaveEffectNoLaterThan: Unbounded[Instant]): Gen[LifecycleForAnId] = inAnyOrder {
-    for {
-      recordingActionFactoriesOverSeveralKinds <-
-      Gen.sequence[Seq[PatchesOfTheSameKindForAnId], PatchesOfTheSameKindForAnId](Seq(patchGenerator(fooProperty1) _, patchGenerator(fooProperty2) _) map (patchesOfTheSameKindForAnIdGenerator(id, seed, bestPatchSelection, eventsHaveEffectNoLaterThan, _)))
-    } yield {
-      val randomBehaviour = new Random(seed)
-      randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralKinds)
-    }
-  }
-
-  def finiteLifecycleForAnIdGenerator(id: FooHistory#Id,
-                                      seed: Long,
-                                      identifiedItemsScope: IdentifiedItemsScope,
-                                      bestPatchSelection: BestPatchSelection,
-                                      eventsHaveEffectNoLaterThan: Unbounded[Instant]): Gen[LifecycleForAnId] = for {
-    recordingActionFactories <- lifecycleForAnIdGenerator(id, seed, bestPatchSelection, eventsHaveEffectNoLaterThan)
-  } yield {
-    def recordingFinalAnnihilation(when: Instant)(patchRecorder: PatchRecorder): Unit = {
-      val patchApplicationDoesNotBreachTheCutoff = Finite(when) <= eventsHaveEffectNoLaterThan
-
-      if (patchApplicationDoesNotBreachTheCutoff) {
-        (identifiedItemsScope.annihilateItemFor[FooHistory](_: FooHistory#Id, _: Instant)(_: TypeTag[FooHistory])).expects(id, when, *).once
-      }
-      patchRecorder.recordAnnihilation[FooHistory](when, id)
-    }
-
-    recordingActionFactories :+ (recordingFinalAnnihilation _)
-  }
-
-  def lifecyclesForAnIdGenerator(id: FooHistory#Id,
-                                 seed: Long,
-                                 identifiedItemsScope: IdentifiedItemsScope,
-                                 bestPatchSelection: BestPatchSelection,
-                                 eventsHaveEffectNoLaterThan: Unbounded[Instant]): Gen[LifecyclesForAnId] = inSequence {
-    val unconstrainedGenerator = for {
-      recordingsItemFactoriesForFiniteLifecycles <- Gen.listOf(finiteLifecycleForAnIdGenerator(id, seed, identifiedItemsScope, bestPatchSelection, eventsHaveEffectNoLaterThan))
-      finalUnboundedLifecycle <- Gen.option(lifecycleForAnIdGenerator(id, seed, bestPatchSelection, eventsHaveEffectNoLaterThan))
-    } yield {
-      val recordingActionFactories = (recordingsItemFactoriesForFiniteLifecycles :\ Seq.empty[RecordingActionFactory]) (_ ++ _)
-
-      finalUnboundedLifecycle match {
-        case Some(finalRecordingActionFactories) =>
-          recordingActionFactories ++ finalRecordingActionFactories
-        case None =>
-          recordingActionFactories
-      }
-    }
-    unconstrainedGenerator
-  }
 
   def recordingActionFactoriesGenerator(seed: Long,
                                         identifiedItemsScope: IdentifiedItemsScope,
                                         bestPatchSelection: BestPatchSelection,
                                         eventsHaveEffectNoLaterThan: Unbounded[Instant]): Gen[Seq[RecordingActionFactory]] = inAnyOrder {
+    def lifecyclesForAnIdGenerator(id: FooHistory#Id): Gen[LifecyclesForAnId] = inSequence {
+      def lifecycleForAnIdGenerator(id: FooHistory#Id): Gen[LifecycleForAnId] = inAnyOrder {
+        def patchesOfTheSameKindForAnIdGenerator(patchGenerator: FooHistory#Id => Gen[AbstractPatch]): Gen[PatchesOfTheSameKindForAnId] = for {
+          patches <- Gen.nonEmptyListOf(patchGenerator(id))
+          initialPatchInLifecycleIsChange <- Gen.oneOf(false, true)
+        } yield {
+          val randomBehaviour = new Random(seed)
+          val clumpsOfPatches = randomBehaviour.splitIntoNonEmptyPieces(patches).force
+
+          val (setupInteractionsWithBestPatchSelection, bestPatches) =
+            (for (clumpOfPatches <- clumpsOfPatches) yield {
+              val bestPatch = randomBehaviour.chooseOneOf(clumpOfPatches)
+
+              def setupInteractionWithBestPatchSelection() {
+                (bestPatchSelection.apply _).expects(clumpOfPatches.toSeq).returns(bestPatch).once
+              }
+
+              setupInteractionWithBestPatchSelection _ -> bestPatch
+            }) unzip
+
+          inSequence {
+            for (setupInteraction <- setupInteractionsWithBestPatchSelection) {
+              setupInteraction()
+            }
+          }
+
+          val changeInsteadOfMeasurementDecisionsInClumps = (initialPatchInLifecycleIsChange #:: Stream.continually(false)) +: Stream.continually(true #:: Stream.continually(false))
+
+          val recordingActionFactories = clumpsOfPatches zip bestPatches zip changeInsteadOfMeasurementDecisionsInClumps map {
+            case ((clumpOfPatches, bestPatch), decisions)  =>
+              // HACK: this next variable and how it is used is truly horrible...
+              var bestPatchIsExpectedToBeAppliedNoLaterThanCutoff: Option[Boolean] = None
+              clumpOfPatches.toSeq zip decisions map {
+                case (patch, makeAChange) =>
+                  def setupInteractionWithBestPatchApplication(patch: AbstractPatch, eventsHaveEffectNoLaterThan: Unbounded[Instant], when: Instant): Unit = {
+                    bestPatchIsExpectedToBeAppliedNoLaterThanCutoff match {
+                      case None => bestPatchIsExpectedToBeAppliedNoLaterThanCutoff = Some(Finite(when) <= eventsHaveEffectNoLaterThan)
+                      case Some(_) =>
+                    }
+
+                    if (bestPatchIsExpectedToBeAppliedNoLaterThanCutoff.get && bestPatches.contains(patch)) {
+                      (patch.apply _).expects(*).once
+                      (patch.checkInvariant _).expects(*).once
+                    } else {
+                      (patch.apply _).expects(*).never
+                      (patch.checkInvariant _).expects(*).never
+                    }
+                  }
+
+                  def recordingChange(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
+                    setupInteractionWithBestPatchApplication(patch, eventsHaveEffectNoLaterThan, when)
+                    patchRecorder.recordPatchFromChange(Finite(when), patch)
+                  }
+
+                  def recordingMeasurement(patch: AbstractPatch)(when: Instant)(patchRecorder: PatchRecorder): Unit = {
+                    setupInteractionWithBestPatchApplication(patch, eventsHaveEffectNoLaterThan, when)
+                    patchRecorder.recordPatchFromMeasurement(Finite(when), patch)
+                  }
+                  if (makeAChange) recordingChange(patch) _ else recordingMeasurement(patch) _
+              }
+          }
+
+          recordingActionFactories.flatten
+        }
+
+        for {
+          recordingActionFactoriesOverSeveralKinds <-
+          Gen.sequence[Seq[PatchesOfTheSameKindForAnId], PatchesOfTheSameKindForAnId](Seq(patchGenerator(fooProperty1) _, patchGenerator(fooProperty2) _) map (patchesOfTheSameKindForAnIdGenerator(_)))
+        } yield {
+          val randomBehaviour = new Random(seed)
+          randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralKinds)
+        }
+      }
+
+      def finiteLifecycleForAnIdGenerator(id: FooHistory#Id): Gen[LifecycleForAnId] = for {
+        recordingActionFactories <- lifecycleForAnIdGenerator(id)
+      } yield {
+        def recordingFinalAnnihilation(when: Instant)(patchRecorder: PatchRecorder): Unit = {
+          val patchApplicationDoesNotBreachTheCutoff = Finite(when) <= eventsHaveEffectNoLaterThan
+
+          if (patchApplicationDoesNotBreachTheCutoff) {
+            (identifiedItemsScope.annihilateItemFor[FooHistory](_: FooHistory#Id, _: Instant)(_: TypeTag[FooHistory])).expects(id, when, *).once
+          }
+          patchRecorder.recordAnnihilation[FooHistory](when, id)
+        }
+
+        recordingActionFactories :+ (recordingFinalAnnihilation _)
+      }
+
+      val unconstrainedGenerator = for {
+        recordingsItemFactoriesForFiniteLifecycles <- Gen.listOf(finiteLifecycleForAnIdGenerator(id))
+        finalUnboundedLifecycle <- Gen.option(lifecycleForAnIdGenerator(id))
+      } yield {
+        val recordingActionFactories = (recordingsItemFactoriesForFiniteLifecycles :\ Seq.empty[RecordingActionFactory]) (_ ++ _)
+
+        finalUnboundedLifecycle match {
+          case Some(finalRecordingActionFactories) =>
+            recordingActionFactories ++ finalRecordingActionFactories
+          case None =>
+            recordingActionFactories
+        }
+      }
+      unconstrainedGenerator
+    }
+
     for {
       ids <- Gen.nonEmptyContainerOf[Set, FooHistory#Id](fooHistoryIdGenerator)
-      recordingActionFactoriesOverSeveralIds <- Gen.sequence[Seq[LifecyclesForAnId], LifecyclesForAnId](ids.toSeq map (lifecyclesForAnIdGenerator(_, seed, identifiedItemsScope, bestPatchSelection, eventsHaveEffectNoLaterThan)))
+      recordingActionFactoriesOverSeveralIds <- Gen.sequence[Seq[LifecyclesForAnId], LifecyclesForAnId](ids.toSeq map (lifecyclesForAnIdGenerator(_)))
     } yield {
       val randomBehaviour = new Random(seed)
       randomBehaviour.pickAlternatelyFrom(recordingActionFactoriesOverSeveralIds)
