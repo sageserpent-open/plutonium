ec1bd666905e63d2db9c5a0f86cba788ad1a5abd
g.j.murphy@sageserpent.com <>
2016-06-21 14:48:02 +0100

    Do not serialize class objects when serializing patches - use the class names instead and reconstruct the class objects on the fly using a cache.

--- src/main/scala-2.11/com/sageserpent/plutonium/Patch.scala
+++ src/main/scala-2.11/com/sageserpent/plutonium/Patch.scala
@@ -6,6 +6,7 @@ import com.sageserpent.plutonium.Patch.MethodPieces
 import net.sf.cglib.core.Signature
 import net.sf.cglib.proxy.MethodProxy
 
+import scala.collection.mutable
 import scalaz.{-\/, \/, \/-}
 
 
@@ -21,15 +22,46 @@ object Patch {
     case _ => -\/(argument)
   }
 
+  private val classNameToClassMap = mutable.Map.empty[String, Class[_]]
+
+  private def registerPrimitiveClass[Primitive](primitiveClass: Class[Primitive]): Unit ={
+    classNameToClassMap(primitiveClass.getName) = primitiveClass
+  }
+
+  registerPrimitiveClass(classOf[Boolean])
+  registerPrimitiveClass(classOf[Char])
+  registerPrimitiveClass(classOf[Byte])
+  registerPrimitiveClass(classOf[Short])
+  registerPrimitiveClass(classOf[Int])
+  registerPrimitiveClass(classOf[Float])
+  registerPrimitiveClass(classOf[Double])
+  registerPrimitiveClass(classOf[Unit])
+
+  private def classFor(className: String): Class[_] = classNameToClassMap.getOrElseUpdate(className, Class.forName(className))
+
   def apply(targetRecorder: Recorder, method: Method, arguments: Array[AnyRef], methodProxy: MethodProxy) = {
-    val methodPieces = MethodPieces(method.getDeclaringClass.asInstanceOf[Class[_ <: Identified]], method.getName, method.getParameterTypes, methodProxy.getSignature.getDescriptor)
+    // Prime 'classNameToClassMap' for patches that are being created as a
+    // result of patch recording, as opposed to being deserialized.
+    val classDeclaringTheMethod = method.getDeclaringClass
+    val nameOfClassDeclaringTheMethod = classDeclaringTheMethod.getName
+
+    //classNameToClassMap(nameOfClassDeclaringTheMethod) = classDeclaringTheMethod
+
+    val parameterTypes = method.getParameterTypes
+    val methodParameterTypeNames = parameterTypes map (_.getName)
+
+/*    for ((parameterTypeName, parameterType) <- methodParameterTypeNames zip parameterTypes){
+      classNameToClassMap(parameterType.getName) = parameterType
+    }*/
+
+    val methodPieces = MethodPieces(nameOfClassDeclaringTheMethod, method.getName, methodParameterTypeNames, methodProxy.getSignature.getDescriptor)
     new Patch(methodPieces,
       targetRecorder.itemReconstitutionData,
       arguments map wrap)
   }
 
-  case class MethodPieces(declaringClassOfMethod: Class[_ <: Identified], methodName: String, methodParameterTypes: Array[Class[_]], signatureDescriptor: String) {
-    def method = declaringClassOfMethod.getMethod(methodName, methodParameterTypes: _*)
+  case class MethodPieces(declaringClassOfMethodName: String, methodName: String, methodParameterTypeNames: Array[String], signatureDescriptor: String) {
+    def method = classFor(declaringClassOfMethodName).getMethod(methodName, methodParameterTypeNames map classFor: _*)
 
     def methodProxyFor(proxyClassOfTarget: Class[_ <: Identified]) = MethodProxy.find(proxyClassOfTarget, new Signature(methodName, signatureDescriptor))
   }
