3d129e085cd75e4670b904075d6b6c0dfec62c8e
Gerard Murphy <g.j.murphy@sageserpent.com>
2017-11-16 22:53:45 +0000

    WIP: this doesn't work, but I like the structure of the code much more. Either go back to using the item cache to handle item reference cycles when deserializing, or fix the issue of separate pools of ids when doing a recursive deserialization from a separate snapshot. Or may be use a reference resolver that can fall back to the item cache ... or something.

--- src/main/scala/com/sageserpent/plutonium/ItemStateStorage.scala
+++ src/main/scala/com/sageserpent/plutonium/ItemStateStorage.scala
@@ -1,114 +1,122 @@
 package com.sageserpent.plutonium
 
+import java.io.ByteArrayOutputStream
+
 import com.esotericsoftware.kryo.factories.{
   ReflectionSerializerFactory,
   SerializerFactory
 }
 import com.esotericsoftware.kryo.io.{Input, Output}
+import com.esotericsoftware.kryo.pool.{KryoFactory, KryoPool}
 import com.esotericsoftware.kryo.serializers.{FieldSerializer, JavaSerializer}
 import com.esotericsoftware.kryo.util.ListReferenceResolver
 import com.esotericsoftware.kryo.{Kryo, ReferenceResolver, Serializer}
-import com.twitter.chill.{KryoBase, KryoPool, ScalaKryoInstantiator}
+import com.twitter.chill.{KryoBase, ScalaKryoInstantiator}
 
 import scala.collection.mutable
 import scala.reflect.runtime.universe.TypeTag
 import scala.util.DynamicVariable
+import resource._
+
+import scala.reflect.runtime.universe
+import scalaz.{-\/, \/, \/-}
 
 object ItemStateStorage {
   import BlobStorage._
 
-  val accessToReconstitutionContext =
-    new DynamicVariable[Option[(Option[ReconstitutionContext],
-                                ReferenceResolver)]](None)
+  val itemDeserializationThreadContextAccess =
+    new DynamicVariable[
+      Option[ReconstitutionContext#ItemDeserializationThreadContext]](None)
 
-  val kryoPoolForRootReferences =
-    KryoPool.withByteArrayOutputStream(
-      40,
-      new ScalaKryoInstantiator {
-        override def newKryo(): KryoBase = {
-          val defaultSerializerFactory =
-            new ReflectionSerializerFactory(classOf[FieldSerializer[_]])
+  val defaultSerializerFactory =
+    new ReflectionSerializerFactory(classOf[FieldSerializer[_]])
 
-          val referenceResolver = reconstitutionContext._2
+  val serializerThatDirectlyEncodesInterItemReferences = {
+    new Serializer[ItemExtensionApi] {
+      val javaSerializer = new JavaSerializer
 
-          val kryo = super.newKryo()
+      override def read(kryo: Kryo,
+                        input: Input,
+                        itemType: Class[ItemExtensionApi]): ItemExtensionApi = {
+        val isForAnInterItemReference = 1 < kryo.getDepth
+        if (isForAnInterItemReference) {
+          val itemId =
+            kryo.readClassAndObject(input)
+          val itemTypeTag = typeTagForClass(
+            kryo.readObject(input, classOf[Class[_]], javaSerializer))
 
-          kryo.setReferenceResolver(referenceResolver)
-
-          kryo.setDefaultSerializer(defaultSerializerFactory)
-
-          val kryoForFallbackSerializers = super.newKryo()
-
-          kryoForFallbackSerializers.setDefaultSerializer(
-            new SerializerFactory {
-              override def makeSerializer(doNotUseThisKryo: Kryo,
-                                          clazz: Class[_]): Serializer[_] =
-                defaultSerializerFactory.makeSerializer(kryo, clazz)
-            })
-
-          val serializerThatDirectlyEncodesInterItemReferences =
-            new Serializer[ItemExtensionApi] {
-              val javaSerializer = new JavaSerializer
-
-              override def read(
-                  kryo: Kryo,
-                  input: Input,
-                  itemType: Class[ItemExtensionApi]): ItemExtensionApi = {
-                val isForAnInterItemReference = 1 < kryo.getDepth
-                if (isForAnInterItemReference) {
-                  val itemId =
-                    kryo.readClassAndObject(input)
-                  val itemTypeTag = typeTagForClass(
-                    kryo.readObject(input, classOf[Class[_]], javaSerializer))
-
-                  val instance: ItemExtensionApi =
-                    reconstitutionContext._1.get
-                      .itemFor[ItemExtensionApi](itemId -> itemTypeTag)
-                  kryo.reference(instance)
-                  instance
-                } else
-                  kryoForFallbackSerializers
-                    .getSerializer(itemType)
-                    .asInstanceOf[Serializer[ItemExtensionApi]]
-                    .read(kryo, input, itemType)
-              }
-
-              override def write(kryo: Kryo,
-                                 output: Output,
-                                 item: ItemExtensionApi): Unit = {
-                val isForAnInterItemReference = 1 < kryo.getDepth
-                if (isForAnInterItemReference) {
-                  kryo.writeClassAndObject(output, item.id)
-                  kryo.writeObject(output, item.getClass, javaSerializer)
-                } else
-                  kryoForFallbackSerializers
-                    .getSerializer(item.getClass)
-                    .asInstanceOf[Serializer[ItemExtensionApi]]
-                    .write(kryo, output, item)
-              }
-            }
-          kryo.addDefaultSerializer(
-            classOf[ItemExtensionApi],
-            serializerThatDirectlyEncodesInterItemReferences)
-
-          kryo
-        }
+          val instance: ItemExtensionApi =
+            itemFor[ItemExtensionApi](itemId -> itemTypeTag)
+          //TODO - do I need this, or is it taken care of in the recursive deserialization that uses the shared reference resolver?
+          // Come to think of it, if we store ids, how would we know if they align from completely separate serializations of several
+          // items?
+          //kryo.reference(instance)
+          // TODO - reinstate the previous line and use some kind of chaining between reference resolvers that works with separate pools of ids
+          // from distinct serializations - so there is some kind of translation of ids or sharing of the same object reference under more than one id?
+          instance
+        } else
+          defaultSerializerFactory
+            .makeSerializer(kryo, itemType) // TODO - cache these serializers.
+            .asInstanceOf[Serializer[ItemExtensionApi]]
+            .read(kryo, input, itemType)
       }
-    )
 
-  private def reconstitutionContext = {
-    accessToReconstitutionContext.value.get
+      override def write(kryo: Kryo,
+                         output: Output,
+                         item: ItemExtensionApi): Unit = {
+        val isForAnInterItemReference = 1 < kryo.getDepth
+        if (isForAnInterItemReference) {
+          kryo.writeClassAndObject(output, item.id)
+          kryo.writeObject(output, item.getClass, javaSerializer)
+        } else
+          defaultSerializerFactory
+            .makeSerializer(kryo, item.getClass) // TODO - cache these serializers.
+            .asInstanceOf[Serializer[ItemExtensionApi]]
+            .write(kryo, output, item)
+      }
+    }
   }
 
-  def snapshotFor[Item: TypeTag](item: Item): SnapshotBlob =
-    accessToReconstitutionContext.withValue(
-      Some(None -> new ListReferenceResolver)) {
-      kryoPoolForRootReferences.toBytesWithClass(item)
+  val kryoInstantiator = new ScalaKryoInstantiator {
+    override def newKryo(): KryoBase = {
+      val kryo = super.newKryo()
+      kryo.setDefaultSerializer(defaultSerializerFactory)
+      kryo.addDefaultSerializer(
+        classOf[ItemExtensionApi],
+        serializerThatDirectlyEncodesInterItemReferences)
+      kryo.setAutoReset(false) // Prevent resets of kryo instances used by recursive deserialization, otherwise the shared reference resolver will be reset too early.
+      kryo
     }
+  }
+
+  val kryoFactory: KryoFactory = () => kryoInstantiator.newKryo()
+
+  val kryoPool = new KryoPool.Builder(kryoFactory).softReferences().build()
+
+  def snapshotFor[Item: TypeTag](item: Item): SnapshotBlob =
+    (for {
+      stream <- makeManagedResource(new ByteArrayOutputStream)(_.close)(
+        List.empty)
+      output <- makeManagedResource(new Output(stream))(_.close)(List.empty)
+      kryo <- makeManagedResource {
+        val kryo = kryoPool.borrow()
+        kryo.reset()
+        kryo.setReferenceResolver(new ListReferenceResolver)
+        kryo
+      }(kryoPool.release)(List.empty)
+    } yield {
+      kryo.writeClassAndObject(output, item)
+      output.flush()
+      stream.toByteArray
+    }).acquireAndGet(identity)
+
+  def itemFor[Item: TypeTag](
+      uniqueItemSpecification: UniqueItemSpecification): Item =
+    itemDeserializationThreadContextAccess.value.get
+      .itemFor(uniqueItemSpecification)
 
   trait ReconstitutionContext extends ItemCache {
     val blobStorageTimeslice: BlobStorage.Timeslice // NOTE: abstracting this allows the prospect of a 'moving' timeslice for use when executing an update plan.
-
     override def itemsFor[Item: TypeTag](id: Any): Stream[Item] =
       for {
         uniqueItemSpecification <- blobStorageTimeslice.uniqueItemQueriesFor(id)
@@ -120,23 +128,44 @@ object ItemStateStorage {
           .uniqueItemQueriesFor[Item]
       } yield itemFor(uniqueItemSpecification)
 
-    private[ItemStateStorage] def itemFor[Item: TypeTag](
-        uniqueItemSpecification: UniqueItemSpecification): Item = {
-      storage
-        .getOrElseUpdate(
-          uniqueItemSpecification, {
-            val snapshot =
-              blobStorageTimeslice.snapshotBlobFor(uniqueItemSpecification)
+    private def itemFor[Item: TypeTag](
+        uniqueItemSpecification: (Any, universe.TypeTag[_])) = {
+      itemDeserializationThreadContextAccess.withValue(
+        Some(new ItemDeserializationThreadContext)) {
+        ItemStateStorage.itemFor(uniqueItemSpecification)
+      }
+    }
 
-            if (accessToReconstitutionContext.value.isEmpty)
-              accessToReconstitutionContext.withValue(
-                Some(Some(this) -> new ListReferenceResolver)) {
-                kryoPoolForRootReferences.fromBytes(snapshot)
-              } else kryoPoolForRootReferences.fromBytes(snapshot)
+    class ItemDeserializationThreadContext {
+      val referenceResolver = new ListReferenceResolver()
 
-          }
-        )
-        .asInstanceOf[Item]
+      private[ItemStateStorage] def itemFor[Item: TypeTag](
+          uniqueItemSpecification: UniqueItemSpecification): Item = {
+        storage
+          .getOrElseUpdate(
+            uniqueItemSpecification, {
+              val snapshot =
+                blobStorageTimeslice.snapshotBlobFor(uniqueItemSpecification)
+
+              def itemFromSnapshot(snapshot: SnapshotBlob) =
+                (for {
+                  input <- makeManagedResource(new Input(snapshot))(_.close)(
+                    List.empty)
+                  kryo <- makeManagedResource {
+                    val kryo = kryoPool.borrow()
+                    kryo.reset()
+                    kryo.setReferenceResolver(referenceResolver)
+                    kryo
+                  }(kryoPool.release)(List.empty)
+                } yield {
+                  kryo.readClassAndObject(input)
+                }).acquireAndGet(identity)
+
+              itemFromSnapshot(snapshot)
+            }
+          )
+          .asInstanceOf[Item]
+      }
     }
 
     class Storage extends mutable.HashMap[UniqueItemSpecification, Any]
