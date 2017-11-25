358577e29919bb889f09a67e225fd9f8f67ad7cf
Gerard Murphy <g.j.murphy@sageserpent.com>
2017-11-25 14:48:46 +0000

    WIP: flesh out 'Timeline' now that some of the lower level pieces are taking shape.

--- src/main/scala/com/sageserpent/plutonium/Timeline.scala
+++ src/main/scala/com/sageserpent/plutonium/Timeline.scala
@@ -3,8 +3,18 @@ package com.sageserpent.plutonium
 import java.time.Instant
 
 import com.sageserpent.americium.Unbounded
+import com.sageserpent.plutonium.BlobStorage.UniqueItemSpecification
+import com.sageserpent.plutonium.WorldImplementationCodeFactoring.{
+  ProxyFactory,
+  QueryCallbackStuff
+}
+import net.bytebuddy.dynamic.DynamicType.Builder
+import net.bytebuddy.implementation.MethodDelegation
 
 import scala.collection.immutable.Map
+import scala.collection.mutable
+import scala.reflect.runtime.universe
+import scala.reflect.runtime.universe.{Super => _, This => _, _}
 
 trait Timeline[EventId] {
   def revise[NewEventId >: EventId](
@@ -18,10 +28,67 @@ trait Timeline[EventId] {
 // TODO - given that we have 'emptyItemCache', I'm not sure if we need this too - let's see how it pans out...
 object emptyTimeline extends TimelineImplementation[Nothing]
 
-class TimelineImplementation[EventId](events: Map[EventId, Event] = Map.empty)
+// TODO - find a home for this...
+object itemStateStorageUsingProxies extends ItemStateStorage {
+  override type ItemSuperType = ItemExtensionApi
+  override val clazzOfItemSuperType = classOf[ItemSuperType]
+
+  override def idFrom(item: ItemSuperType) = item.id
+
+  override def createItemFor[Item](
+      uniqueItemSpecification: UniqueItemSpecification) = {
+    import QueryCallbackStuff._
+
+    val (id, itemTypeTag) = uniqueItemSpecification
+
+    val proxyFactory = new ProxyFactory[AcquiredState] {
+      override val isForRecordingOnly = false
+
+      override val stateToBeAcquiredByProxy: AcquiredState =
+        new AcquiredState {
+          val _id = id
+
+          def itemReconstitutionData: Recorder#ItemReconstitutionData[Item] =
+            id -> itemTypeTag.asInstanceOf[TypeTag[Item]]
+
+          def itemsAreLocked: Boolean = true
+        }
+
+      override val acquiredStateClazz = classOf[AcquiredState]
+
+      override val additionalInterfaces: Array[Class[_]] =
+        QueryCallbackStuff.additionalInterfaces
+      override val cachedProxyConstructors
+        : mutable.Map[universe.Type, (universe.MethodMirror, Class[_])] =
+        QueryCallbackStuff.cachedProxyConstructors
+
+      override protected def configureInterceptions(
+          builder: Builder[_]): Builder[_] =
+        builder
+          .method(matchCheckedReadAccess)
+          .intercept(MethodDelegation.to(checkedReadAccess))
+          .method(matchIsGhost)
+          .intercept(MethodDelegation.to(isGhost))
+          .method(matchMutation)
+          .intercept(MethodDelegation.to(mutation))
+          .method(matchRecordAnnihilation)
+          .intercept(MethodDelegation.to(recordAnnihilation))
+          .method(matchInvariantCheck)
+          .intercept(MethodDelegation.to(checkInvariant))
+    }
+
+    proxyFactory.constructFrom(id)
+  }
+}
+
+class TimelineImplementation[EventId](events: Map[EventId, Event] = Map.empty,
+                                      blobStorage: BlobStorage[EventId] =
+                                        BlobStorageInMemory.apply[EventId]())
     extends Timeline[EventId] {
   override def revise[NewEventId >: EventId](
       events: Map[NewEventId, Option[Event]]) = {
+
+    // PLAN: need to create a new blob storage and the mysterious high-level lifecycle set with incremental patch application abstraction.
 
     new TimelineImplementation(
       events = (this.events
@@ -35,5 +102,9 @@ class TimelineImplementation[EventId](events: Map[EventId, Event] = Map.empty)
   override def retainUpTo(when: Unbounded[Instant]) =
     ??? // TODO - support experimental worlds.
 
-  override def itemCacheAt(when: Unbounded[Instant]) = ???
+  override def itemCacheAt(when: Unbounded[Instant]) =
+    new itemStateStorageUsingProxies.ReconstitutionContext {
+      override val blobStorageTimeslice: BlobStorage.Timeslice =
+        blobStorage.timeSlice(when)
+    }
 }
