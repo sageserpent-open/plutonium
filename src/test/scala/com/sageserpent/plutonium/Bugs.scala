e5a099a766f6429ebc85e82a34f80074c52203b9
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-02-18 16:05:29 +0000

    Add another bug report, this is similar to the first one but demonstrates a possible variant in the failure.

--- src/test/scala/com/sageserpent/plutonium/Bugs.scala
+++ src/test/scala/com/sageserpent/plutonium/Bugs.scala
@@ -14,7 +14,7 @@ class WorldEfficientInMemoryImplementationBugs
     with GeneratorDrivenPropertyChecks
     with WorldSpecSupport
     with WorldEfficientInMemoryImplementationResource {
-  "events that should have been revised" should "not contribute history to an item when the revised event refers to another item" in {
+  "events that should have been revised" should "no longer contribute history to an item, even when the revised event refers to another item" in {
     forAll(worldResourceGenerator) { worldResource =>
       val firstItemId = "Number One"
 
@@ -23,7 +23,7 @@ class WorldEfficientInMemoryImplementationBugs
       val Seq(eventBeingRevised: EventId,
               firstFinalEventForFirstItem: EventId) = 1 to 2
 
-      val sharedTimeOfFinalAndObsoleteEvent = 1L
+      val timeOfObsoleteEvent = Instant.ofEpochSecond(1L)
 
       val expectedFinalValue = -20
 
@@ -32,29 +32,31 @@ class WorldEfficientInMemoryImplementationBugs
       worldResource acquireAndGet { world =>
         world.revise(
           Map(
-            firstFinalEventForFirstItem -> Some(
-              Change
-                .forOneItem[IntegerHistory](Instant.ofEpochSecond(
-                  sharedTimeOfFinalAndObsoleteEvent))(firstItemId, { item =>
-                  item.integerProperty = expectedFinalValue
-                })),
-            eventBeingRevised -> Some(
-              Change
-                .forOneItem[IntegerHistory](Instant.ofEpochSecond(
-                  sharedTimeOfFinalAndObsoleteEvent))(firstItemId, { item =>
+            eventBeingRevised -> Some(Change
+              .forOneItem[IntegerHistory](timeOfObsoleteEvent)(firstItemId, {
+                item =>
                   item.integerProperty = 734634
-                }))
+              }))
           ),
           sharedAsOf
         )
 
         world.revise(
-          eventBeingRevised,
-          Change
-            .forOneItem[MoreSpecificFooHistory](Instant.ofEpochSecond(2L))(
-              secondItemId, { item =>
-                item.property1 = "Kingston Bagpuize"
-              }),
+          Map(
+            firstFinalEventForFirstItem -> Some(
+              Change
+                .forOneItem[IntegerHistory](timeOfObsoleteEvent plusMillis 1)(
+                  firstItemId, { item =>
+                    item.integerProperty = expectedFinalValue
+                  })),
+            eventBeingRevised ->
+              Some(
+                Change
+                  .forOneItem[MoreSpecificFooHistory](
+                    timeOfObsoleteEvent plusMillis 2)(secondItemId, { item =>
+                    item.property1 = "Kingston Bagpuize"
+                  }))
+          ),
           sharedAsOf
         )
 
@@ -66,7 +68,62 @@ class WorldEfficientInMemoryImplementationBugs
           .loneElement
           .datums should contain theSameElementsAs (Seq(expectedFinalValue))
       }
+    }
+  }
 
+  "events that should have been revised" should "no longer contribute history to an item, even when the revised event refers to another item - with a twist" in {
+    forAll(worldResourceGenerator) { worldResource =>
+      val firstItemId = "Number One"
+
+      val secondItemId = "Number Two"
+
+      val Seq(eventBeingRevised: EventId,
+              firstFinalEventForFirstItem: EventId) = 1 to 2
+
+      val sharedTimeOfFinalAndObsoleteEvent = Instant.ofEpochSecond(1L)
+
+      val expectedFinalValue = -20
+
+      val sharedAsOf = Instant.ofEpochSecond(0)
+
+      worldResource acquireAndGet { world =>
+        world.revise(
+          Map(
+            firstFinalEventForFirstItem -> Some(
+              Change
+                .forOneItem[IntegerHistory](sharedTimeOfFinalAndObsoleteEvent)(
+                  firstItemId, { item =>
+                    item.integerProperty = expectedFinalValue
+                  })),
+            eventBeingRevised -> Some(
+              Change
+                .forOneItem[IntegerHistory](sharedTimeOfFinalAndObsoleteEvent)(
+                  firstItemId, { item =>
+                    item.integerProperty = 734634
+                  }))
+          ),
+          sharedAsOf
+        )
+
+        world.revise(
+          eventBeingRevised,
+          Change
+            .forOneItem[MoreSpecificFooHistory](
+              sharedTimeOfFinalAndObsoleteEvent plusMillis 1)(secondItemId, {
+              item =>
+                item.property1 = "Kingston Bagpuize"
+            }),
+          sharedAsOf
+        )
+
+        val scope =
+          world.scopeFor(PositiveInfinity[Instant](), world.nextRevision)
+
+        scope
+          .render(Bitemporal.withId[IntegerHistory](firstItemId))
+          .loneElement
+          .datums should contain theSameElementsAs (Seq(expectedFinalValue))
+      }
     }
   }
 }
