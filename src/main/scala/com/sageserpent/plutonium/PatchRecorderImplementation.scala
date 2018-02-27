facee1386b362fba2f1282055f9491e5a8f4b23f
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-02-27 21:28:54 +0000

    Revert "Questionable fix that sorts out most the test failure occurrences seen in both the reference and efficient world implementations. I'm concerned that the test suite is missing coverage of multiple lifecycles. To be investigated..."
    This reverts commit 260e85e179fc4535e4e89ae9343800f16d326de6.

--- src/main/scala/com/sageserpent/plutonium/PatchRecorderImplementation.scala
+++ src/main/scala/com/sageserpent/plutonium/PatchRecorderImplementation.scala
@@ -292,7 +292,7 @@ abstract class PatchRecorderImplementation[EventId](
       patchRepresentingTheEvent
 
     val reconstitutionDataToItemStateMap =
-      patchToItemStatesMap(bestPatch)
+      patchToItemStatesMap.remove(bestPatch).get
 
     for ((UniqueItemSpecification(id, _), itemState) <- reconstitutionDataToItemStateMap) {
       if (itemState.itemWouldConflictWithEarlierLifecyclePriorTo > sequenceIndexForBestPatch) {
