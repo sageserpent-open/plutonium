b8add1e44618a1015952ffe17010b49408527a2a
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-03-13 15:25:05 +0000

    Added second test into 'LifecyclesStateSpec' - this fails, as expected. Still have to sort out dependencies in addition to changing the SUT.

--- src/test/scala/com/sageserpent/plutonium/LifecyclesStateSpec.scala
+++ src/test/scala/com/sageserpent/plutonium/LifecyclesStateSpec.scala
@@ -7,7 +7,42 @@ import org.scalatest.exceptions.TestFailedException
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Inspectors, Matchers}
 
+import scala.collection.immutable
 import scala.collection.immutable.TreeMap
+import scala.util.Random
+
+import scalaz.std.stream
+
+object LifecyclesStateSpec {
+  implicit class UpdatePlanEnhancement[EventId](
+      val updatePlan: UpdatePlan[EventId])
+      extends AnyVal {
+    def supersededBy(
+        supersedingUpdatePlan: UpdatePlan[EventId]): UpdatePlan[EventId] = {
+      val newAndCorrectedEvents = supersedingUpdatePlan.updates.values
+        .flatMap(_.flatMap(_._1))
+        .toSet
+      val eventsToKnockOut
+        : Set[EventId] = supersedingUpdatePlan.annulments ++ newAndCorrectedEvents
+
+      val (knockedOutUpdates, preservedUpdates) = updatePlan.updates.partition {
+        case (_, itemStateUpdates) =>
+          eventsToKnockOut
+            .intersect(itemStateUpdates.map(_._1).reduce(_ union _))
+            .nonEmpty
+      }
+
+      val eventsActuallyKnockedOut =
+        knockedOutUpdates.values.flatMap(_.flatMap(_._1)).toSet
+
+      val newAnnulments = (updatePlan.annulments diff eventsToKnockOut) ++ (supersedingUpdatePlan.annulments diff eventsActuallyKnockedOut)
+
+      val newUpdates = preservedUpdates ++ supersedingUpdatePlan.updates
+
+      UpdatePlan(annulments = newAnnulments, updates = newUpdates)
+    }
+  }
+}
 
 class LifecyclesStateSpec
     extends FlatSpec
@@ -15,34 +50,7 @@ class LifecyclesStateSpec
     with GeneratorDrivenPropertyChecks
     with NoShrinking
     with WorldSpecSupport {
-  // TODO - presumably there would be some kind of compatibility test with 'PatchRecorder'?
-
-  // TODO - so if one 'LifecyclesState' was mutated into another by a series of revisions, it would make the same cumulative update plan
-  // as if all the revisions were rolled together as one - or at least it would have the same effect. Hmm, need to think about that more precisely.
-
-  // 1. If I feed in new events that refer to a bunch of item ids, then I know that the updates generated will only refer to a subset of those ids plus those booked in via prior revisions.
-
-  // 2. Something to do with working out the type of a lifecycle's item.
-
-  // 3. Preservation of event ordering in the update plan.
-
-  // 4. Events must be a subset of the ones booked in.
-
-  // 5. Something to do with measurements.
-
-  // 6. Something to do with annihilations.
-
-  // 7. Start with a bunch of events at various times, some of which collide. Scramble them up and revise an empty lifecycles state. The update plan that results
-  // should refer to patches that come from the events, and these should be correctly ordered wrt to the the events that made them.
-
-  // 8. Similar to #7, but intersperse obsolete events - all of the obsolete events must either be explicitly annulled or knocked out by a subsequent final
-  // update. What gets through should have the same effect as if one big revision was made with the final events.
-
-  // Test case strategy: make set of ids, and for each id, maintain a sequence of maps of disjoint item types. It is permissible to knock out one item type
-  // key and add in another, as long the invariant is maintained that the keys are disjoint types. The associated values are times when an event happens - this could
-  // be a change or a measurement. Removing a key models annihilating an item. The mapped times are updated in non strictly increasing order. Whenever an actual event
-  // is made from a maplet, the type of the item may be replaced by some subclass, as long as all such substitutions are themselves supertypes of some common class
-  // that is not just 'Null'.
+  import LifecyclesStateSpec._
 
   type EventId = Int
 
@@ -91,6 +99,58 @@ class LifecyclesStateSpec
                 s"for id: $historyId at: $queryWhen, $message"))
           }
       }
+    }
+  }
+
+  "Booking in events over several revisions with other events that are revised or annulled" should "result in the same update plan as booking in the events via a single block revision" in {
+    forAll(
+      recordingsGroupedByIdGenerator(forbidAnnihilations = false)
+        .map(_.flatMap(_.events) map (_._2)),
+      nonConflictingRecordingsGroupedByIdGenerator.map(
+        _.flatMap(_.events) map (_._2)),
+      seedGenerator,
+      unboundedInstantGenerator
+    ) { (events, obsoleteEvents, seed, queryWhen) =>
+      def harvestUpdatePlan(updatePlan: UpdatePlan[EventId])
+        : (LifecyclesState.Dependencies, UpdatePlan[EventId]) =
+        noDependencies -> updatePlan
+
+      val eventsInOneBlock: Map[EventId, Some[Event]] = TreeMap(
+        // Use 'TreeMap' as it won't rearrange the key ordering. We do this purely
+        // to ensure that events that occur at the same time won't be shuffled as
+        // they go into the map wrt the expected histories.
+        events.zipWithIndex map {
+          case (event, index) => index -> Some(event)
+        }: _*)
+
+      val updatePlanResultingFromBlockBooking: UpdatePlan[EventId] =
+        noLifecyclesState[EventId]()
+          .revise(eventsInOneBlock, harvestUpdatePlan)
+          ._2
+
+      val random = new Random(seed)
+
+      val severalRevisionBookingsWithObsoleteEventsThrownIn
+        : List[Map[EventId, Option[Event]]] =
+        intersperseObsoleteEvents(random, events, obsoleteEvents) map (
+            booking => TreeMap(booking.map(_.swap): _*)) toList
+
+      val incrementalUpdatePlans
+        : immutable.Seq[UpdatePlan[EventId]] = stream.unfold(noLifecyclesState[
+        EventId]() -> severalRevisionBookingsWithObsoleteEventsThrownIn) {
+        case (lifecyclesState, Nil) => None
+        case (lifecyclesState, booking :: remainingBookings) =>
+          val (revisedLifecyclesState, updatePlan) =
+            lifecyclesState.revise(booking, harvestUpdatePlan)
+          Some(updatePlan, revisedLifecyclesState -> remainingBookings)
+      }
+
+      val cumulativeUpdatePlan
+        : UpdatePlan[EventId] = incrementalUpdatePlans reduce (
+          (first,
+           second) => first supersededBy second)
+
+      cumulativeUpdatePlan should be(updatePlanResultingFromBlockBooking)
     }
   }
 }
