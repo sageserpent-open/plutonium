fe71dcca77cf990a7a56ca8db0be42bc5b5613dc
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-03-21 16:41:31 +0000

    Recreate the reconstitution context anew as the update plan jumps forward in physical time. This is necessary for incremental updating, as items that are only read from have to be repeatedly reconstituted from the blob storage at different points in time.

--- src/main/scala/com/sageserpent/plutonium/UpdatePlan.scala
+++ src/main/scala/com/sageserpent/plutonium/UpdatePlan.scala
@@ -32,106 +32,100 @@ case class UpdatePlan[EventId](
       initialMicroRevisionBuilder.build()
     }
 
-    val identifiedItemAccess = new IdentifiedItemAccess
-    with itemStateStorageUsingProxies.ReconstitutionContext {
-      override def reconstitute(
-          uniqueItemSpecification: UniqueItemSpecification) =
-        if (forgottenItemSpecifications.contains(uniqueItemSpecification)) {
-          forgottenItemSpecifications -= uniqueItemSpecification
-          fallbackItemFor[Any](uniqueItemSpecification)
-        } else itemFor[Any](uniqueItemSpecification)
-
-      private val forgottenItemSpecifications =
-        mutable.Set.empty[UniqueItemSpecification]
-
-      override def forget(
-          uniqueItemSpecification: UniqueItemSpecification): Unit = {
-        val item = reconstitute(uniqueItemSpecification)
-          .asInstanceOf[AnnihilationHook]
-          .recordAnnihilation()
-        forgottenItemSpecifications += uniqueItemSpecification
-      }
-
-      private var blobStorageTimeSlice =
-        microRevisedBlobStorage.timeSlice(NegativeInfinity())
-
-      var allItemsAreLocked = true
-
-      private val itemsMutatedSinceLastSnapshotHarvest =
-        mutable.Map.empty[UniqueItemSpecification, ItemExtensionApi]
-
-      def resetSourceTimesliceTo(when: Unbounded[Instant]) = {
-        blobStorageTimeSlice = microRevisedBlobStorage.timeSlice(when)
-      }
-
-      override def blobStorageTimeslice: BlobStorage.Timeslice[SnapshotBlob] =
-        blobStorageTimeSlice
-
-      override protected def createItemFor[Item](
-          _uniqueItemSpecification: UniqueItemSpecification,
-          lifecycleUUID: UUID) = {
-        import QueryCallbackStuff.{AcquiredState, proxyFactory}
-
-        val stateToBeAcquiredByProxy: AcquiredState =
-          new AcquiredState {
-            val uniqueItemSpecification: UniqueItemSpecification =
-              _uniqueItemSpecification
-            def itemIsLocked: Boolean = allItemsAreLocked
-            def recordMutation(item: ItemExtensionApi): Unit = {
-              itemsMutatedSinceLastSnapshotHarvest.update(
-                item.uniqueItemSpecification,
-                item)
-            }
-          }
-
-        implicit val typeTagForItem: TypeTag[Item] =
-          _uniqueItemSpecification.typeTag.asInstanceOf[TypeTag[Item]]
-
-        val item = proxyFactory.constructFrom[Item](stateToBeAcquiredByProxy)
-
-        item
-          .asInstanceOf[AnnihilationHook]
-          .setLifecycleUUID(lifecycleUUID)
-
-        item
-      }
-
-      override protected def fallbackItemFor[Item](
-          uniqueItemSpecification: UniqueItemSpecification): Item = {
-        val item =
-          createAndStoreItem[Item](uniqueItemSpecification, UUID.randomUUID())
-        itemsMutatedSinceLastSnapshotHarvest.update(
-          uniqueItemSpecification,
-          item.asInstanceOf[ItemExtensionApi])
-        item
-      }
-
-      override protected def fallbackRelatedItemFor[Item](
-          uniqueItemSpecification: UniqueItemSpecification): Item = {
-        val item =
-          createItemFor[Item](uniqueItemSpecification, UUID.randomUUID())
-        item.asInstanceOf[AnnihilationHook].recordAnnihilation()
-        item
-      }
-
-      def harvestSnapshots(): Map[UniqueItemSpecification, SnapshotBlob] = {
-        val result = itemsMutatedSinceLastSnapshotHarvest map {
-          case (uniqueItemSpecification, item) =>
-            val snapshotBlob = itemStateStorageUsingProxies.snapshotFor(item)
-
-            uniqueItemSpecification -> snapshotBlob
-        } toMap
-
-        itemsMutatedSinceLastSnapshotHarvest.clear()
-
-        result
-      }
-    }
-
     for {
       (when, itemStateUpdates: Seq[(Set[EventId], ItemStateUpdate)]) <- updates
     } {
-      identifiedItemAccess.resetSourceTimesliceTo(when)
+      val identifiedItemAccess = new IdentifiedItemAccess
+      with itemStateStorageUsingProxies.ReconstitutionContext {
+        override def reconstitute(
+            uniqueItemSpecification: UniqueItemSpecification) =
+          if (forgottenItemSpecifications.contains(uniqueItemSpecification)) {
+            forgottenItemSpecifications -= uniqueItemSpecification
+            fallbackItemFor[Any](uniqueItemSpecification)
+          } else itemFor[Any](uniqueItemSpecification)
+
+        private val forgottenItemSpecifications =
+          mutable.Set.empty[UniqueItemSpecification]
+
+        override def forget(
+            uniqueItemSpecification: UniqueItemSpecification): Unit = {
+          val item = reconstitute(uniqueItemSpecification)
+            .asInstanceOf[AnnihilationHook]
+            .recordAnnihilation()
+          forgottenItemSpecifications += uniqueItemSpecification
+        }
+
+        private val blobStorageTimeSlice =
+          microRevisedBlobStorage.timeSlice(when)
+
+        var allItemsAreLocked = true
+
+        private val itemsMutatedSinceLastSnapshotHarvest =
+          mutable.Map.empty[UniqueItemSpecification, ItemExtensionApi]
+
+        override def blobStorageTimeslice: BlobStorage.Timeslice[SnapshotBlob] =
+          blobStorageTimeSlice
+
+        override protected def createItemFor[Item](
+            _uniqueItemSpecification: UniqueItemSpecification,
+            lifecycleUUID: UUID) = {
+          import QueryCallbackStuff.{AcquiredState, proxyFactory}
+
+          val stateToBeAcquiredByProxy: AcquiredState =
+            new AcquiredState {
+              val uniqueItemSpecification: UniqueItemSpecification =
+                _uniqueItemSpecification
+              def itemIsLocked: Boolean = allItemsAreLocked
+              def recordMutation(item: ItemExtensionApi): Unit = {
+                itemsMutatedSinceLastSnapshotHarvest.update(
+                  item.uniqueItemSpecification,
+                  item)
+              }
+            }
+
+          implicit val typeTagForItem: TypeTag[Item] =
+            _uniqueItemSpecification.typeTag.asInstanceOf[TypeTag[Item]]
+
+          val item = proxyFactory.constructFrom[Item](stateToBeAcquiredByProxy)
+
+          item
+            .asInstanceOf[AnnihilationHook]
+            .setLifecycleUUID(lifecycleUUID)
+
+          item
+        }
+
+        override protected def fallbackItemFor[Item](
+            uniqueItemSpecification: UniqueItemSpecification): Item = {
+          val item =
+            createAndStoreItem[Item](uniqueItemSpecification, UUID.randomUUID())
+          itemsMutatedSinceLastSnapshotHarvest.update(
+            uniqueItemSpecification,
+            item.asInstanceOf[ItemExtensionApi])
+          item
+        }
+
+        override protected def fallbackRelatedItemFor[Item](
+            uniqueItemSpecification: UniqueItemSpecification): Item = {
+          val item =
+            createItemFor[Item](uniqueItemSpecification, UUID.randomUUID())
+          item.asInstanceOf[AnnihilationHook].recordAnnihilation()
+          item
+        }
+
+        def harvestSnapshots(): Map[UniqueItemSpecification, SnapshotBlob] = {
+          val result = itemsMutatedSinceLastSnapshotHarvest map {
+            case (uniqueItemSpecification, item) =>
+              val snapshotBlob = itemStateStorageUsingProxies.snapshotFor(item)
+
+              uniqueItemSpecification -> snapshotBlob
+          } toMap
+
+          itemsMutatedSinceLastSnapshotHarvest.clear()
+
+          result
+        }
+      }
 
       {
         val microRevisionBuilder = microRevisedBlobStorage.openRevision()
