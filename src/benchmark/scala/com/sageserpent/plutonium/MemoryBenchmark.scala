a702d567de0d5fa8b8ee721d983f8f1d9561df34
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-06-25 14:56:21 +0000

    Split the old benchmark object into two separate benchmarks sharing the same activity - one for timing, another for memory usage.

---
+++ src/benchmark/scala/com/sageserpent/plutonium/MemoryBenchmark.scala
@@ -0,0 +1,17 @@
+package com.sageserpent.plutonium
+
+import org.scalameter.api.{Aggregator, Bench, Context, Executor, Gen, exec}
+import org.scalameter.picklers.noPickler._
+
+object MemoryBenchmark extends Bench.Forked[Double] with Benchmark {
+  val sizes = Gen.range("Number of bookings")(0, 4000, 100)
+
+  override def measurer = new Executor.Measurer.MemoryFootprint
+
+  override def aggregator: Aggregator[Double] = Aggregator.average
+  override def defaultConfig: Context         = Context(exec.independentSamples -> 1)
+
+  performance of "Bookings" in {
+    using(sizes) config (exec.benchRuns -> 5, exec.jvmflags -> List("-Xmx3G")) in activity
+  }
+}
