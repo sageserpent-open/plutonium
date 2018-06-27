b51ba1d233d076f7757857986077aa82201c5b2a
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-06-27 16:30:25 +0000

    Eliminate a potential source of quadratic scaling in the benchmark's own code and add in querying at random revisions and times.

--- src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
@@ -4,19 +4,26 @@ import java.time.Instant
 
 import com.sageserpent.americium.randomEnrichment._
 
+import scala.util.Random
+
 trait Benchmark {
+  implicit class Enhancement(randomBehaviour: Random) {
+    def chooseOneOfRange(range: Range): Int =
+      range(randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(range.size))
+  }
+
   def activity(size: Int): World = {
     if (0 == size % 300) println(s"*** Size: $size")
     val randomBehaviour = new scala.util.Random(1368234L)
 
-    val eventIds = 0 until 1 + (size / 10)
+    val eventIds: Range = 0 until 1 + (size / 10)
 
-    val idSet = 0 until 1 + (size / 5)
+    val idSet: Range = 0 until 1 + (size / 5)
 
     val world = new WorldEfficientInMemoryImplementation()
 
     for (step <- 0 until size) {
-      val eventId = randomBehaviour.chooseOneOf(eventIds)
+      val eventId = randomBehaviour.chooseOneOfRange(eventIds)
 
       val probabilityOfNotBackdatingAnEvent = 0 < randomBehaviour
         .chooseAnyNumberFromZeroToOneLessThan(3)
@@ -31,9 +38,9 @@ trait Benchmark {
         .chooseAnyNumberFromZeroToOneLessThan(5)
 
       if (probabilityOfBookingANewOrCorrectingEvent) {
-        val oneId = randomBehaviour.chooseOneOf(idSet)
+        val oneId = randomBehaviour.chooseOneOfRange(idSet)
 
-        val anotherId = randomBehaviour.chooseOneOf(idSet)
+        val anotherId = randomBehaviour.chooseOneOfRange(idSet)
 
         world.revise(
           eventId,
@@ -48,6 +55,20 @@ trait Benchmark {
           Instant.now()
         )
       } else world.annul(eventId, Instant.now())
+
+      val onePastQueryRevision =
+        randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
+          1 + world.nextRevision)
+
+      val queryTime = Instant.ofEpochSecond(
+        3600L * randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
+          1 + theHourFromTheStart))
+
+      val scope = world.scopeFor(queryTime, onePastQueryRevision)
+
+      val queryId = randomBehaviour.chooseOneOfRange(idSet)
+
+      scope.render(Bitemporal.withId[Thing](queryId)).force
     }
 
     world
