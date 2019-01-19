2a8b45aa9b2308b8008f3611669f82670b5ba401
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-01-19 16:25:48 +0000

    Reset the test dials back to what was on the parent commit on 'master'.

--- src/test/scala/com/sageserpent/plutonium/WorldStateSharingBehaviours.scala
+++ src/test/scala/com/sageserpent/plutonium/WorldStateSharingBehaviours.scala
@@ -496,7 +496,7 @@ class WorldStateSharingSpecUsingWorldRedisBasedImplementation
   val redisServerPort: Int = 6451
 
   val testParameters: Test.Parameters =
-    Test.Parameters.defaultVerbose.withMaxSize(30).withMinSuccessfulTests(15)
+    Test.Parameters.defaultVerbose.withMaxSize(30).withMinSuccessfulTests(50)
 
   val numberOfConcurrentQueriesPerRevision: Revision = 20
 
