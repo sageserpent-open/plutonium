6342c696953c20e211d0fbc3ae0c0ef0b466a400
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-03-30 09:13:12 +0000

    Use caching in 'classFromType', as it is hammered by 'ImmutableObjectStorage' to perform runtime type checks on retrieved objects, as well as being used all over the Scala API entry points.

--- src/main/scala/com/sageserpent/plutonium/classFromType.scala
+++ src/main/scala/com/sageserpent/plutonium/classFromType.scala
@@ -2,12 +2,30 @@ package com.sageserpent.plutonium
 
 import scala.reflect.runtime.{currentMirror, universe}
 import universe.typeOf
+import scalacache._
+import scalacache.caffeine._
+import scalacache.memoization._
+import scalacache.modes.sync._
+
+import scala.concurrent.duration._
 
 object classFromType {
+  private val cacheTimeToLive = Some(10 minutes)
+
+  implicit val clazzCache: Cache[Class[Any]] = CaffeineCache[Class[Any]](
+    CacheConfig.defaultCacheConfig.copy(
+      memoization = MemoizationConfig(
+        (fullClassName: String,
+         constructorParameters: IndexedSeq[IndexedSeq[Any]],
+         methodName: String,
+         parameters: IndexedSeq[IndexedSeq[Any]]) => parameters.head.toString)))
+
   def apply[Item](reflectedType: universe.Type): Class[Item] =
-    (if (typeOf[Any] =:= reflectedType) classOf[Any]
-     else
-       currentMirror
-         .runtimeClass(reflectedType))
-      .asInstanceOf[Class[Item]]
+    memoizeSync(cacheTimeToLive) {
+      if (typeOf[Any] =:= reflectedType) classOf[Any]
+      else
+        currentMirror
+          .runtimeClass(reflectedType)
+          .asInstanceOf[Class[Any]]
+    }.asInstanceOf[Class[Item]]
 }
