18a3cd8ed75b0e68d9aa5308c86771f1883b0d91
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-14 12:59:01 +0000

    Some clumsy hacks to the benchmark so that it benchmarks 'WorldH2StorageImplementation' and does so in a reasonable time for development work. Ths commit will be reworked later...

--- src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
@@ -2,7 +2,9 @@ package com.sageserpent.plutonium
 
 import java.time.Instant
 
+import cats.effect.IO
 import com.sageserpent.americium.randomEnrichment._
+import com.sageserpent.plutonium.curium.H2Resource
 
 import scala.util.Random
 
@@ -13,64 +15,69 @@ trait Benchmark {
   }
 
   def activity(size: Int): World = {
-    if (0 == size % 300) println(s"*** Size: $size")
+    println(s"*** Size: $size")
     val randomBehaviour = new scala.util.Random(1368234L)
 
     val eventIds: Range = 0 until 1 + (size / 10)
 
     val idSet: Range = 0 until 1 + (size / 5)
 
-    val world = new WorldEfficientInMemoryImplementation()
+    val world: IO[World] = H2Resource.transactorResource.use(transactor =>
+      IO {
+        val world = new WorldH2StorageImplementation(transactor)
 
-    for (step <- 0 until size) {
-      val eventId = randomBehaviour.chooseOneOfRange(eventIds)
+        for (step <- 0 until size) {
+          val eventId = randomBehaviour.chooseOneOfRange(eventIds)
 
-      val probabilityOfNotBackdatingAnEvent = 0 < randomBehaviour
-        .chooseAnyNumberFromZeroToOneLessThan(3)
+          val probabilityOfNotBackdatingAnEvent = 0 < randomBehaviour
+            .chooseAnyNumberFromZeroToOneLessThan(3)
 
-      val theHourFromTheStart =
-        if (probabilityOfNotBackdatingAnEvent) step
-        else
-          step - randomBehaviour
-            .chooseAnyNumberFromOneTo(step / 3 min 20)
+          val theHourFromTheStart =
+            if (probabilityOfNotBackdatingAnEvent) step
+            else
+              step - randomBehaviour
+                .chooseAnyNumberFromOneTo(step / 3 min 20)
 
-      val probabilityOfBookingANewOrCorrectingEvent = 0 < randomBehaviour
-        .chooseAnyNumberFromZeroToOneLessThan(5)
+          val probabilityOfBookingANewOrCorrectingEvent = 0 < randomBehaviour
+            .chooseAnyNumberFromZeroToOneLessThan(5)
 
-      if (probabilityOfBookingANewOrCorrectingEvent) {
-        val oneId = randomBehaviour.chooseOneOfRange(idSet)
+          if (probabilityOfBookingANewOrCorrectingEvent) {
+            val oneId = randomBehaviour.chooseOneOfRange(idSet)
 
-        val anotherId = randomBehaviour.chooseOneOfRange(idSet)
+            val anotherId = randomBehaviour.chooseOneOfRange(idSet)
 
-        world.revise(
-          eventId,
-          Change.forTwoItems[Thing, Thing](
-            Instant.ofEpochSecond(3600L * theHourFromTheStart))(
-            oneId,
-            anotherId,
-            (oneThing, anotherThing) => {
-              oneThing.property1 = step
-              oneThing.referTo(anotherThing)
-            }),
-          Instant.now()
-        )
-      } else world.annul(eventId, Instant.now())
+            world.revise(
+              eventId,
+              Change.forTwoItems[Thing, Thing](
+                Instant.ofEpochSecond(3600L * theHourFromTheStart))(
+                oneId,
+                anotherId,
+                (oneThing, anotherThing) => {
+                  oneThing.property1 = step
+                  oneThing.referTo(anotherThing)
+                }),
+              Instant.now()
+            )
+          } else world.annul(eventId, Instant.now())
 
-      val onePastQueryRevision =
-        randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
-          1 + world.nextRevision)
+          val onePastQueryRevision =
+            randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
+              1 + world.nextRevision)
 
-      val queryTime = Instant.ofEpochSecond(
-        3600L * randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
-          1 + theHourFromTheStart))
+          val queryTime = Instant.ofEpochSecond(
+            3600L * randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
+              1 + theHourFromTheStart))
 
-      val scope = world.scopeFor(queryTime, onePastQueryRevision)
+          val scope = world.scopeFor(queryTime, onePastQueryRevision)
 
-      val queryId = randomBehaviour.chooseOneOfRange(idSet)
+          val queryId = randomBehaviour.chooseOneOfRange(idSet)
 
-      scope.render(Bitemporal.withId[Thing](queryId)).force
-    }
+          scope.render(Bitemporal.withId[Thing](queryId)).force
+        }
 
-    world
+        world // NASTY HACK - allow the world to escape the resource scope, so that memory footprints can be taken.
+    })
+
+    world.unsafeRunSync()
   }
 }
--- src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
@@ -3,10 +3,10 @@ package com.sageserpent.plutonium
 import org.scalameter.api.{Bench, Gen, exec}
 
 object TimeBenchmark extends Bench.ForkedTime with Benchmark {
-  val sizes = Gen.range("Number of bookings")(0, 20100, 100)
+  val sizes = Gen.range("Number of bookings")(0, 100, 5)
 
   performance of "Bookings" in {
-    using(sizes) config (exec.benchRuns -> 5, exec.jvmflags -> List("-Xmx3G")) in activity
+    using(sizes) config (exec.benchRuns -> 3, exec.jvmflags -> List("-Xmx3G")) in activity
   }
 
 }
