a674aec4d38a01c3a7bf261cc71d5af49c102736
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-22 17:05:23 +0000

    Beef up the timer benchmark to explore further beyond 500 steps.

--- src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
@@ -3,7 +3,7 @@ package com.sageserpent.plutonium
 import org.scalameter.api.{Bench, Gen, exec}
 
 object TimeBenchmark extends Bench.ForkedTime with Benchmark {
-  val sizes = Gen.range("Number of bookings")(0, 700, 50)
+  val sizes = Gen.range("Number of bookings")(500, 1500, 50)
 
   performance of "Bookings" in {
     using(sizes) config (exec.maxWarmupRuns -> 1, exec.benchRuns -> 1, exec.jvmflags -> List(
