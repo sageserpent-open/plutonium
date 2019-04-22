86f8ca9c9c32cd0784137c4d23e8dc0165f2c41f
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-22 11:19:34 +0000

    Scouting with the time benchmark against the in-memory H2 database.

--- src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
@@ -3,11 +3,11 @@ package com.sageserpent.plutonium
 import org.scalameter.api.{Bench, Gen, exec}
 
 object TimeBenchmark extends Bench.ForkedTime with Benchmark {
-  val sizes = Gen.range("Number of bookings")(0, 2000, 25)
+  val sizes = Gen.range("Number of bookings")(0, 700, 50)
 
   performance of "Bookings" in {
-    using(sizes) config (exec.maxWarmupRuns -> 5, exec.benchRuns -> 3, exec.jvmflags -> List(
-      "-Xmx5G")) in activity
+    using(sizes) config (exec.maxWarmupRuns -> 1, exec.benchRuns -> 1, exec.jvmflags -> List(
+      "-Xmx8G")) in activity
   }
 
 }
