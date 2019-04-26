de91ef885bec1477218f121aa0903c04313592a2
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-26 13:09:41 +0000

    Bump of the number of steps on the time benchmark.

--- src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/TimeBenchmark.scala
@@ -3,11 +3,11 @@ package com.sageserpent.plutonium
 import org.scalameter.api.{Bench, Gen, exec}
 
 object TimeBenchmark extends Bench.ForkedTime with Benchmark {
-  val sizes = Gen.range("Number of bookings")(500, 1500, 50)
+  val sizes = Gen.range("Number of bookings")(500, 3000, 50)
 
   performance of "Bookings" in {
     using(sizes) config (exec.maxWarmupRuns -> 1, exec.benchRuns -> 1, exec.jvmflags -> List(
-      "-Xmx8G")) in activity
+      "-Xmx10G")) in activity
   }
 
 }
