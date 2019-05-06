01bd48f07b1bae4f57e18635e52e1e4a6c1efef4
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-05-06 13:56:16 +0100

    Debugging support - this commit will be reverted.

--- src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
@@ -32,6 +32,8 @@ trait Benchmark {
           val world = new WorldH2StorageImplementation(connectionPool)
 
           for (step <- 0 until size) {
+            println(
+              "--------------------------------------------------------------------------")
             val eventId = randomBehaviour.chooseOneOfRange(eventIds)
 
             val probabilityOfNotBackdatingAnEvent = 0 < randomBehaviour
@@ -51,6 +53,8 @@ trait Benchmark {
 
               val anotherId = randomBehaviour.chooseOneOfRange(idSet)
 
+              println("****** REVISING ******")
+
               world.revise(
                 eventId,
                 Change.forTwoItems[Thing, Thing](
@@ -63,7 +67,11 @@ trait Benchmark {
                   }),
                 Instant.now()
               )
-            } else world.annul(eventId, Instant.now())
+            } else {
+              println("++++++ ANNULLING ++++++")
+
+              world.annul(eventId, Instant.now())
+            }
 
             val onePastQueryRevision =
               randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
@@ -76,6 +84,8 @@ trait Benchmark {
             val scope = world.scopeFor(queryTime, onePastQueryRevision)
 
             val queryId = randomBehaviour.chooseOneOfRange(idSet)
+
+            println("@@@@@@ QUERYING @@@@@@")
 
             scope.render(Bitemporal.withId[Thing](queryId)).force
 
