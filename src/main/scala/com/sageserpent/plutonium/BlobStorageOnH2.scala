737fe20b6116e886da872e9400bf99cb30eeb95e
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-05-23 20:00:40 +0100

    WIP: the database schema and how to query the database is starting to take shape, loads more to do...

--- src/main/scala/com/sageserpent/plutonium/BlobStorageOnH2.scala
+++ src/main/scala/com/sageserpent/plutonium/BlobStorageOnH2.scala
@@ -1,13 +1,21 @@
 package com.sageserpent.plutonium
 
-import java.util.UUID
+import java.time.Instant
 
 import cats.effect.IO
+import cats.implicits._
+import com.sageserpent.americium.{
+  Finite,
+  NegativeInfinity,
+  PositiveInfinity,
+  Unbounded
+}
+import com.sageserpent.plutonium.ItemStateStorage.SnapshotBlob
 import com.sageserpent.plutonium.curium.DBResource
+import com.twitter.chill.{KryoPool, KryoSerializer}
 import scalikejdbc._
 
-import com.sageserpent.plutonium.ItemStateStorage.SnapshotBlob
-
+import scala.collection.immutable.{SortedMap, TreeMap}
 import scala.collection.mutable
 
 //noinspection SqlNoDataSourceInspection
@@ -19,7 +27,7 @@ object BlobStorageOnH2 {
     BlobStorageOnH2(connectionPool,
                     sentinelLineageId,
                     initialRevision,
-                    Map.empty)
+                    TreeMap.empty)
 
   def setupDatabaseTables(connectionPool: ConnectionPool): IO[Unit] =
     DBResource(connectionPool)
@@ -32,46 +40,48 @@ object BlobStorageOnH2 {
               // to be careful when editing this.
               sql"""
               CREATE TABLE Lineage(
-                LineageId               BIGINT                    IDENTITY(0, 1),
-                MaximumRevision         INTEGER                   NOT NULL
+                LineageId       BIGINT  IDENTITY(0, 1),
+                MaximumRevision INTEGER NOT NULL
               )
       """.update.apply()
 
+              // TODO - add check about what is and isn't null...
               sql"""
               CREATE TABLE Recording(
-                TimeCategory            INT                       NOT NULL,
-                EventTimeCategory       INT                       NOT NULL,
-                EventTime               TIMESTAMP WITH TIME ZONE  NOT NULL,
-                EventRevision           INT                       NOT NULL,
-                EventTiebreaker         INT                       NOT NULL,
-                IntraEventIndex         INT                       NOT NULL,
-                LineageId               BIGINT                    REFERENCES Lineage(LineageId),
-                Revision                INTEGER                   NOT NULL,
-                PRIMARY KEY (TimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex),
+                ItemStateUpdateTimeCategory INT                       NOT NULL,
+                EventTimeCategory           INT                       NULL,
+                EventTime                   TIMESTAMP WITH TIME ZONE  NULL,
+                EventRevision               INT                       NULL,
+                EventTiebreaker             INT                       NULL,
+                IntraEventIndex             INT                       NULL,
+                LineageId                   BIGINT                    REFERENCES Lineage(LineageId),
+                Revision                    INTEGER                   NOT NULL,
+                PRIMARY KEY (ItemStateUpdateTimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex, LineageId, Revision),
                 CHECK Revision < ALL(SELECT MaximumRevision FROM Lineage WHERE Lineage.LineageId = LineageId),
-                CHECK TimeCategory IN (-1, 0, 1),
+                CHECK ItemStateUpdateTimeCategory IN (-1, 0, 1),
                 CHECK EventTimeCategory IN (-1, 0, 1)
               )
       """.update.apply()
 
+              // TODO - add check about what is and isn't null...
               sql"""
               CREATE TABLE Snapshot(
-                ItemId                  BINARY                    NOT NULL,
-                ItemClass               BINARY                    NOT NULL,
-                TimeCategory            INT                       NOT NULL,
-                EventTimeCategory       INT                       NOT NULL,
-                EventTime               TIMESTAMP WITH TIME ZONE  NOT NULL,
-                EventRevision           INT                       NOT NULL,
-                EventTiebreaker         INT                       NOT NULL,
-                IntraEventIndex         INT                       NOT NULL,
-                LineageId               BIGINT                    REFERENCES Lineage(LineageId),
-                Revision                INTEGER                   NOT NULL,
-                Payload                 BLOB                      NOT NULL,
-                PRIMARY KEY (ItemId, ItemClass, TimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex),
-                FOREIGN KEY (TimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex)
-                  REFERENCES Recording(TimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex),
+                ItemId                      BINARY                    NOT NULL,
+                ItemClass                   BINARY                    NOT NULL,
+                ItemStateUpdateTimeCategory INT                       NOT NULL,
+                EventTimeCategory           INT                       NULL,
+                EventTime                   TIMESTAMP WITH TIME ZONE  NULL,
+                EventRevision               INT                       NULL,
+                EventTiebreaker             INT                       NULL,
+                IntraEventIndex             INT                       NULL,
+                LineageId                   BIGINT                    REFERENCES Lineage(LineageId),
+                Revision                    INTEGER                   NOT NULL,
+                Payload                     BLOB                      NULL,
+                PRIMARY KEY (ItemId, ItemClass, ItemStateUpdateTimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex, LineageId, Revision),
+                FOREIGN KEY (ItemStateUpdateTimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex, LineageId, Revision)
+                  REFERENCES Recording(ItemStateUpdateTimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex, LineageId, Revision),
                 CHECK Revision < ALL(SELECT MaximumRevision FROM Lineage WHERE Lineage.LineageId = LineageId),
-                CHECK TimeCategory IN (-1, 0, 1),
+                CHECK ItemStateUpdateTimeCategory IN (-1, 0, 1),
                 CHECK EventTimeCategory IN (-1, 0, 1)
               )
       """.update.apply()
@@ -94,14 +104,89 @@ object BlobStorageOnH2 {
   val initialRevision: Revision = 0
 
   type LineageId = Long
+
+  def lessThanOrEqualTo(when: Unbounded[Instant]): SQLSyntax =
+    // NOTE: take advantage that the event time category
+    // can only take values from the set {-1, 0, 1}.
+    when match {
+      case NegativeInfinity() =>
+        sqls"EventTimeCategory = -1"
+      case Finite(unlifted) =>
+        sqls"(EventTimeCategory < 0 OR EventTimeCategory = 0 AND EventTime <= $unlifted)"
+      case PositiveInfinity() =>
+        sqls"EventTimeCategory <= 1"
+    }
+
+  def lessThan(when: Unbounded[Instant]): SQLSyntax =
+    // NOTE: take advantage that the event time category
+    // can only take values from the set {-1, 0, 1}.
+    when match {
+      case NegativeInfinity() =>
+        sqls"FALSE"
+      case Finite(unlifted) =>
+        sqls"(EventTimeCategory < 0 OR EventTimeCategory = 0 AND EventTime < $unlifted)"
+      case PositiveInfinity() =>
+        sqls"EventTimeCategory < 1"
+    }
+
+  def equalTo(when: Unbounded[Instant]): SQLSyntax =
+    // NOTE: take advantage that the event time category
+    // can only take values from the set {-1, 0, 1}.
+    when match {
+      case NegativeInfinity() =>
+        sqls"EventTimeCategory = -1"
+      case Finite(unlifted) =>
+        sqls"(EventTimeCategory = 0 AND EventTime = $unlifted)"
+      case PositiveInfinity() =>
+        sqls"EventTimeCategory = 1"
+    }
+
+  def lessThanOrEqualTo(when: ItemStateUpdateTime): SQLSyntax =
+    // NOTE: 'when' is flattened into one of three alternative tuples; these are then subject to
+    // lexicographic ordering, but taking advantage that the item state update time category can
+    // only take values from the set {-1, 0, 1}.
+    when match {
+      case LowerBoundOfTimeslice(when) =>
+        sqls"(ItemStateUpdateTimeCategory = -1 AND ${lessThanOrEqualTo(when)})"
+      case ItemStateUpdateKey(
+          (when, eventRevision, eventOrderingTiebreakerIndex),
+          intraEventIndex) =>
+        //ItemStateUpdateTimeCategory, EventTimeCategory, EventTime, EventRevision, EventTiebreaker, IntraEventIndex
+        sqls"""(ItemStateUpdateTimeCategory = -1 OR
+                                          ItemStateUpdateTimeCategory = 0 AND (${lessThan(
+          when)} OR
+                                            (${equalTo(when)} AND (EventRevision < $eventRevision OR 
+                                              (EventRevision = $eventRevision AND (EventTiebreaker < $eventOrderingTiebreakerIndex OR
+                                                (EventTiebreaker = $eventOrderingTiebreakerIndex AND IntraEventIndex <= $intraEventIndex)))))))"""
+      case UpperBoundOfTimeslice(when) =>
+        sqls"(ItemStateUpdateTimeCategory < 1 OR ItemStateUpdateTimeCategory = 1 AND ${lessThanOrEqualTo(when)})"
+    }
+
+  def matchingSnapshot(lineageId: LineageId,
+                       revision: Revision,
+                       when: ItemStateUpdateTime,
+                       includePayload: Boolean): SQLSyntax = {
+    val payloadSelection = if (includePayload) sqls", Payload" else sqls""
+
+    // TODO - nested query to ensure that what is selected has the maximum permitted revision for that lineage id...
+
+    sqls"""
+      SELECT ItemId, ItemClass${payloadSelection} FROM SNAPSHOT
+      WHERE LineageId = $lineageId AND Revision <= $revision AND
+      ${lessThanOrEqualTo(when)}
+      """
+  }
+
+  val kryoPool: KryoPool =
+    KryoPool.withByteArrayOutputStream(40, KryoSerializer.registered)
 }
 
 case class BlobStorageOnH2(
     connectionPool: ConnectionPool,
     lineageId: BlobStorageOnH2.LineageId,
     revision: BlobStorageOnH2.Revision,
-    ancestralBranchpoints: Map[BlobStorageOnH2.LineageId,
-                               BlobStorageOnH2.Revision])(
+    ancestralBranchpoints: SortedMap[BlobStorageOnH2.LineageId,
+                                     BlobStorageOnH2.Revision])(
     override implicit val timeOrdering: Ordering[ItemStateUpdateTime])
     extends Timeline.BlobStorage {
   thisBlobStorage =>
@@ -110,11 +195,15 @@ case class BlobStorageOnH2(
   private def isHeadOfLineage(): IO[Boolean] =
     DBResource(connectionPool).use(db =>
       IO {
-        db localTx { implicit session: DBSession =>
-          sql"""
+        db localTx {
+          implicit session: DBSession =>
+            // NOTE: the sentinel lineage id is always branched from, never extended; as
+            // it is not actually present in 'Lineage', the fallback below is false to
+            // cause 'build()' to force a new lineage branch each time an empty blob storage
+            // is revised.
+            sql"""
              SELECT MaximumRevision = $revision FROM Lineage WHERE LineageId = $lineageId
-           """.map(_.boolean(0)).single().apply().getOrElse(true)
-          false
+           """.map(_.boolean(0)).single().apply().getOrElse(false)
         }
     })
 
@@ -153,8 +242,59 @@ case class BlobStorageOnH2(
     new RevisionBuilderImplementation
   }
 
-  override def timeSlice(when: ItemStateUpdateTime, inclusive: Boolean)
-    : BlobStorage.Timeslice[ItemStateStorage.SnapshotBlob] = ???
+  override def timeSlice(
+      when: ItemStateUpdateTime,
+      inclusive: Boolean): BlobStorage.Timeslice[SnapshotBlob] =
+    new BlobStorage.Timeslice[SnapshotBlob] {
+      private def snapshotBlobFor[Item](itemId: Option[Any],
+                                        when: ItemStateUpdateTime,
+                                        inclusive: Boolean)
+        : IO[(UniqueItemSpecification, Option[SnapshotBlob])] = ???
+
+      override def uniqueItemQueriesFor[Item](
+          clazz: Class[Item]): Stream[UniqueItemSpecification] = {
+        val branchPoints
+          : Map[LineageId, Revision] = ancestralBranchpoints + (lineageId -> revision)
+
+        DBResource(connectionPool)
+          .use(
+            db =>
+              branchPoints.toStream
+                .traverse {
+                  case (lineageId: LineageId, revision: Revision) =>
+                    IO {
+                      db localTx {
+                        implicit session: DBSession =>
+                          sql"${matchingSnapshot(lineageId, revision, when, includePayload = false)}"
+                            .map(
+                              resultSet =>
+                                kryoPool.fromBytes(resultSet.bytes("ItemId"),
+                                                   classOf[Any]) -> kryoPool
+                                  .fromBytes(resultSet.bytes("ItemClass"),
+                                             classOf[Class[_]]))
+                            .single()
+                            .apply()
+                      }
+                    }
+              }
+          )
+          .unsafeRunSync()
+          .flatten
+          .distinct
+          .map {
+            case (itemId, itemClazz) =>
+              UniqueItemSpecification(itemId, itemClazz)
+          }
+      }
+
+      override def uniqueItemQueriesFor[Item](
+          uniqueItemSpecification: UniqueItemSpecification)
+        : Stream[UniqueItemSpecification] = ???
+
+      override def snapshotBlobFor(
+          uniqueItemSpecification: UniqueItemSpecification)
+        : Option[SnapshotBlob] = ???
+    }
 
   override def retainUpTo(when: ItemStateUpdateTime): Timeline.BlobStorage =
     ???
