49e566c0215495992eb364e364cacdfc4b973e93
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-06-26 14:11:50 +0100

    Modify the benchmark to force longer transitive closures in queries, and introduce timing of individual sections of code.

--- src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/Benchmark.scala
@@ -6,9 +6,8 @@ import cats.effect.IO
 import com.sageserpent.americium.randomEnrichment._
 import com.sageserpent.plutonium.curium.H2ViaScalikeJdbcDatabaseSetupResource
 
-import scala.util.Random
-
 import scala.concurrent.duration._
+import scala.util.Random
 
 trait Benchmark
     extends H2ViaScalikeJdbcDatabaseSetupResource
@@ -19,12 +18,11 @@ trait Benchmark
   }
 
   def activity(size: Int): World = {
-    println(s"*** Size: $size")
     val randomBehaviour = new scala.util.Random(1368234L)
 
     val eventIds: Range = 0 until 1 + (size / 10)
 
-    val idSet: Range = 0 until 1 + (size / 5)
+    val idSet: Range = 0 until 1 + (size / 500)
 
     val startTime = Deadline.now
 
@@ -53,46 +51,44 @@ trait Benchmark
 
               val anotherId = randomBehaviour.chooseOneOfRange(idSet)
 
-              world.revise(
-                eventId,
-                Change.forTwoItems[Thing, Thing](
-                  Instant.ofEpochSecond(3600L * theHourFromTheStart))(
-                  oneId,
-                  anotherId,
-                  (oneThing, anotherThing) => {
-                    oneThing.property1 = step
-                    oneThing.referTo(anotherThing)
-                  }),
-                Instant.now()
-              )
-            } else world.annul(eventId, Instant.now())
+              Timer.withCategory("REVISING") {
+                world.revise(
+                  eventId,
+                  Change.forTwoItems[Thing, Thing](
+                    Instant.ofEpochSecond(3600L * theHourFromTheStart))(
+                    oneId,
+                    anotherId,
+                    (oneThing, anotherThing) => {
+                      oneThing.property1 = step
+                      oneThing.referTo(anotherThing)
+                    }),
+                  Instant.now()
+                )
+              }
+            } else
+              Timer.withCategory("ANNULLING") {
+                world.annul(eventId, Instant.now())
+              }
 
             val onePastQueryRevision =
               randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
                 1 + world.nextRevision)
 
-            val queryTime = Instant.ofEpochSecond(
-              3600L * randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(
-                1 + theHourFromTheStart))
+            val queryTime = Instant.ofEpochSecond(3600L * step)
 
-            val scope = world.scopeFor(queryTime, onePastQueryRevision)
+            val transitiveClosureSize = Timer.withCategory("QUERYING") {
+              val scope = world.scopeFor(queryTime, onePastQueryRevision)
 
-            val queryId = randomBehaviour.chooseOneOfRange(idSet)
+              val queryId = randomBehaviour.chooseOneOfRange(idSet)
 
-            val transitiveClosureSize = scope
-              .render(Bitemporal.withId[Thing](queryId))
-              .force
-              .headOption
-              .fold(0)(_.transitiveClosure)
-
-            if (step % 50 == 0) {
-              val currentTime = Deadline.now
-
-              val duration = currentTime - startTime
-
-              println(
-                s"Step: $step, duration: ${duration.toMillis} milliseconds, transitive closure size: $transitiveClosureSize")
+              scope
+                .render(Bitemporal.withId[Thing](queryId))
+                .force
+                .headOption
+                .fold(0)(_.transitiveClosure)
             }
+
+            if (0 == step % 50) Timer.sampleAndPrintResults(s"$step")
           }
 
           world // NASTY HACK - allow the world to escape the resource scope, so that memory footprints can be taken.
@@ -104,6 +100,6 @@ trait Benchmark
 
 object benchmarkApplication extends Benchmark {
   def main(args: Array[String]): Unit = {
-    activity(3000000)
+    activity(500000)
   }
 }
